0ca57f3a23b7f896e25233ef2151093a01970273
Aaron Graham <aarograh@umich.edu>
2013-02-26 13:51:47 -0500

    Able to write rank-2 array of StringTypes to HDF5 file

--- src/FileType_HDF5.f90
+++ src/FileType_HDF5.f90
@@ -151,12 +151,15 @@ MODULE FileType_HDF5
       PROCEDURE,PASS :: write_c1_helper
       !> Write: character string, rank 1
       PROCEDURE,PASS :: write_c1
+      !> Write: character string, rank 2 helper
+      PROCEDURE,PASS :: write_c2_helper
       !> Write: character string, rank 2
-      ! PROCEDURE,PASS :: write_c2
+      PROCEDURE,PASS :: write_c2
       !> Write data to the file as a dataset
       GENERIC :: write => write_d0,write_d1,write_d2,write_d3,write_s0, &
       write_s1,write_s2,write_s3,write_l0,write_l1,write_l2,write_l3,write_i0, &
-      write_i1,write_i2,write_i3,write_c0,write_c1_helper,write_c1 !,write_c2
+      write_i1,write_i2,write_i3,write_c0,write_c1_helper,write_c1, &
+      write_c2_helper,write_c2
       !> Read: real(SDK), rank 1
       PROCEDURE,PASS :: read_d1
       !> Read: real(SDK), rank 2
@@ -3096,7 +3099,7 @@ MODULE FileType_HDF5
 !
 !-------------------------------------------------------------------------------
     SUBROUTINE write_c1_helper(this,dsetname,data,gdims_in)
-      CHARACTER(LEN=*),PARAMETER :: MyName='writec1_HDF5FileType'
+      CHARACTER(LEN=*),PARAMETER :: MyName='writec1helper_HDF5FileType'
       CLASS(HDF5FileType),INTENT(INOUT) :: this
       CHARACTER(LEN=*),INTENT(IN) :: dsetname
       TYPE(StringType),ALLOCATABLE,INTENT(IN) :: data(:)
@@ -3298,6 +3301,215 @@ MODULE FileType_HDF5
       ENDIF
 #endif
     ENDSUBROUTINE write_c1
+!
+!-------------------------------------------------------------------------------
+    SUBROUTINE write_c2_helper(this,dsetname,data,gdims_in)
+      CHARACTER(LEN=*),PARAMETER :: MyName='writec2helper_HDF5FileType'
+      CLASS(HDF5FileType),INTENT(INOUT) :: this
+      CHARACTER(LEN=*),INTENT(IN) :: dsetname
+      TYPE(StringType),ALLOCATABLE,INTENT(IN) :: data(:,:)
+      INTEGER(SIK) :: length_max,i,j
+      INTEGER(SIK),DIMENSION(2),INTENT(IN),OPTIONAL :: gdims_in
+      
+      length_max=0
+      DO j=1,SIZE(data,1)
+        DO i=1,SIZE(data,2)
+          length_max=MAXVAL(([LEN_TRIM(data(j,i)),length_max]))
+        ENDDO
+      ENDDO
+      
+      IF(PRESENT(gdims_in))THEN
+        CALL this%write(dsetname,data,length_max,gdims_in)
+      ELSE
+        CALL this%write(dsetname,data,length_max)
+      ENDIF
+      
+    ENDSUBROUTINE write_c2_helper
+
+!
+!-------------------------------------------------------------------------------
+!> @brief Write a StringType to dataset
+    SUBROUTINE write_c2(this,dsetname,data,length_max,gdims_in)
+      CHARACTER(LEN=*),PARAMETER :: myName='writec2_HDF5FileType'
+      INTEGER(SIK) :: i,j,k
+      CLASS(HDF5FileType),INTENT(INOUT) :: this
+      CHARACTER(LEN=*),INTENT(IN) :: dsetname
+      TYPE(StringType),ALLOCATABLE,INTENT(IN) :: data(:,:)
+      INTEGER(SIK),INTENT(IN) :: length_max
+      CHARACTER(LEN=length_max) :: datas
+      CHARACTER, ALLOCATABLE :: datac(:)
+      CHARACTER(LEN=SIZE(data)) :: string_number
+      CHARACTER(LEN=MAX_PATH_LENGTH) :: path,path_string,path_shape
+      INTEGER(SIK),DIMENSION(2),INTENT(IN),OPTIONAL :: gdims_in
+      INTEGER(SIK),ALLOCATABLE :: shape_data(:)
+#ifdef MPACT_HAVE_HDF5
+      INTEGER(HSIZE_T),DIMENSION(2) :: gdims
+      INTEGER(HSIZE_T),DIMENSION(1) :: ldims,offset,one
+      INTEGER(HID_T),PARAMETER :: rank=1
+      
+      INTEGER :: error
+      INTEGER(HID_T) :: dspace_id,dset_id,gspace_id,plist_id
+
+      ! Make sure the object is initialized
+      IF(.NOT.this%isinit)THEN
+        CALL this%e%raiseError(myName//': File object not initialized.')
+      ENDIF
+
+      ! Check that the file is writable. Best to catch this before HDF5 does.
+      IF(.NOT.this%isWrite())THEN
+        CALL this%e%raiseError(myName//': File is readonly!')
+        RETURN
+      ENDIF
+      
+      ! set one to ones. This is usually used for more complicated parallel
+      ! chunking schemes, but we are doing a simplified case
+      one=1
+
+      ! Convert the path name
+      path = convertPath(dsetname)
+      DO k=1,SIZE(data,1)
+        DO i=1,SIZE(data,2)
+
+          ! Convert StringType array element (k,i) to character vector
+          datas=TRIM(data(k,i))
+          ALLOCATE(datac(LEN(datas)))
+          DO j=1,SIZE(datac)
+            datac(j)=datas(j:j)
+          ENDDO
+      
+          ! stash offset
+          offset(1) = LBOUND(datac,1)-1
+
+          ! Determine the dimensions for the dataspace
+          ldims=SHAPE(datac)
+      
+          ! Store the dimensions from global if present
+          IF(PRESENT(gdims_in))THEN
+            gdims=gdims_in
+          ENDIF
+      
+          !Create an HDF5 parameter list for the dataset creation.
+          CALL h5pcreate_f(H5P_DATASET_CREATE_F,plist_id,error)
+          IF(error /= 0)THEN
+            CALL this%e%raiseError(myName//': Could not create parameter list.')
+          ENDIF
+      
+          ! Create the dataspace
+#ifdef HAVE_MPI
+          ! Make sure that the global dims are present if needed
+          IF (this%pe%rank == 0)THEN
+            IF(.NOT.PRESENT(gdims_in))THEN
+              CALL this%e%raiseError(myName//': For parallel,write, global '//&
+                'dimensions are required.')
+            ENDIF
+          ENDIF
+          CALL h5pset_chunk_f(plist_id,rank,ldims,error)
+          IF(error /= 0)THEN
+            CALL this%e%raiseError(myName//': Could not set chunk.')
+          ENDIF
+#endif
+          ! Global dataspace
+          ! ldims used since each element is written individually
+          CALL h5screate_simple_f(rank,ldims,gspace_id,error)
+          IF(error /= 0)THEN
+            CALL this%e%raiseError(myName//': Could not create dataspace.')
+          ENDIF
+      
+          ! Local dataspace
+          CALL h5screate_simple_f(rank,ldims,dspace_id,error)
+          IF(error /= 0)THEN
+            CALL this%e%raiseError(myName//': Could not create dataspace.')
+          ENDIF
+        
+          ! Create path for each StringType element
+          WRITE(string_number,FMT="(i0,a1,i0)") k,",",i
+          WRITE(path_string,*) TRIM(path)//"("//TRIM(ADJUSTL(string_number))// &
+                  ")"
+          path_string=ADJUSTL(path_string)
+
+          ! Create the dataset
+          CALL h5dcreate_f(this%file_id, path_string, H5T_NATIVE_CHARACTER, &
+                          gspace_id, dset_id,error)
+          IF(error /= 0)THEN
+            CALL this%e%raiseError(myName//': Could not create dataset.')
+          ENDIF
+      
+          ! Destroy the property list
+          CALL h5pclose_f(plist_id,error)
+          IF(error /= 0)THEN
+            CALL this%e%raiseError(myName//': Could not close parameter list.')
+          ENDIF
+      
+          ! Select the global dataspace for the dataset
+          CALL h5dget_space_f(dset_id,gspace_id,error)
+          IF(error /= 0)THEN
+            CALL this%e%raiseError(myName//': Could not select global '//&
+                    'dataspace for the dataset.')
+          ENDIF
+      
+          ! Create a property list for the write operation
+          CALL h5pcreate_f(H5P_DATASET_XFER_F,plist_id,error)
+          IF(error /= 0)THEN
+            CALL this%e%raiseError(myName//': Could not create property '//&
+              'list for write operation.')
+          ENDIF
+#ifdef HAVE_MPI
+          ! Set to parallel write
+          CALL h5pset_dxpl_mpio_f(plist_id,H5FD_MPIO_COLLECTIVE_F,error)
+          IF(error /= 0)THEN
+            CALL this%e%raiseError(myName//': Could not set parallel write.')
+          ENDIF
+      
+          ! Select the hyperslab
+          CALL h5sselect_hyperslab_f(gspace_id,H5S_SELECT_SET_F,offset,one, &
+                                      error,one,ldims)
+          IF(error /= 0)THEN
+            CALL this%e%raiseError(myName//': Could not select hyperslab.')
+          ENDIF
+#endif
+      
+          ! Write to the dataset
+          CALL h5dwrite_f(dset_id, H5T_NATIVE_CHARACTER, datac, ldims, error, &
+                          dspace_id,gspace_id,plist_id)
+          IF(error /= 0)THEN
+            CALL this%e%raiseError(myName//': Could not write to the dataset.')
+          ENDIF
+          DEALLOCATE(datac)
+
+          ! Close the dataset
+          CALL h5dclose_f(dset_id,error)
+          IF(error /= 0)THEN
+            CALL this%e%raiseError(myName//': Could not close the dataset.')
+          ENDIF
+      
+        ENDDO
+      ENDDO
+      
+      ! Create dataset to store shape of array
+      WRITE(path_shape,*) TRIM(path)//":SHAPE"
+      path_shape=ADJUSTL(path_shape)
+      ALLOCATE(shape_data(SIZE(SHAPE(data))))
+      shape_data=SHAPE(data)
+      CALL this%write(path_shape,shape_data)
+      DEALLOCATE(shape_data)
+      
+
+      ! Close the dataspace
+      CALL h5sclose_f(dspace_id,error)
+      IF(error /= 0)THEN
+        CALL this%e%raiseError(myName//': Could not close the dataspace.')
+      ENDIF
+      CALL h5sclose_f(gspace_id,error)
+      IF(error /= 0)THEN
+        CALL this%e%raiseError(myName//': Could not close the dataspace.')
+      ENDIF
+      
+      CALL h5pclose_f(plist_id,error)
+      IF(error /= 0)THEN
+        CALL this%e%raiseError(myName//': Could not close the parameter list.')
+      ENDIF
+#endif
+    ENDSUBROUTINE write_c2
 !
 !-------------------------------------------------------------------------------
     SUBROUTINE read_d1(this,dsetname,data)
--- unit_tests/testHDF5FileType/testHDF5FileType.f90
+++ unit_tests/testHDF5FileType/testHDF5FileType.f90
@@ -78,7 +78,7 @@ PROGRAM testHDF5
       INTEGER(SIK) :: testI0
       LOGICAL(SBK) :: testL0
       TYPE(StringType) :: testC0
-      TYPE(StringType),ALLOCATABLE :: testC1(:)!,testC2(:,:)
+      TYPE(StringType),ALLOCATABLE :: testC1(:),testC2(:,:)
       INTEGER(SIK) :: i,j,k
 
       ALLOCATE(testD1(10))
@@ -94,7 +94,7 @@ PROGRAM testHDF5
       ALLOCATE(testI2(5,5))
       ALLOCATE(testI3(3,3,3))
       ALLOCATE(testC1(3))
-!      ALLOCATE(testC2(2,2))
+      ALLOCATE(testC2(2,2))
 
       testD0=42.123456789_SDK
       testD1=[(i,i=1,SIZE(testD1))]
@@ -121,8 +121,8 @@ PROGRAM testHDF5
       ENDDO
       testC0='Scalar String Test'
       testC1(1)='String 1';testC1(2)='String 2';testC1(3)='String 3'
-!      testC2(1,1)='String 1,1';testC2(1,2)='String1,2';testC2(2,1)='String 2,1'
-!      testC2(2,2)='String2,2';
+      testC2(1,1)='String 1,1';testC2(1,2)='String 1,2';testC2(2,1)='String 2,1'
+      testC2(2,2)='String 2,2';
     
       ! Create a RW access file. Existing file overwritten
       CALL h5%init('writetest.h5','NEW')
@@ -153,11 +153,12 @@ PROGRAM testHDF5
       CALL h5%write('groupL->memL2',testL2,SHAPE(testL2))
       CALL h5%write('groupL->memL3',testL3,SHAPE(testL3))
       CALL h5%mkdir('groupC')
-      CALL h5%write('groupC->memC',testC0)
+      CALL h5%write('groupC->memC0',testC0)
       CALL h5%write('groupC->memC1',testC1,SHAPE(testC1))
+      CALL h5%write('groupC->memC2',testC2,SHAPE(testC2))
 
       DEALLOCATE(testD1,testD2,testD3,testR1,testR2,testR3,testI1,testI2, &
-            testI3,testL1,testL2,testL3,testC1)
+            testI3,testL1,testL2,testL3,testC1,testC2)
 
       CALL h5%clear()
       ASSERT(.NOT.h5%isinit,'HDF5 object not properly cleared!')
