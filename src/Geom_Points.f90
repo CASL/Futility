521c6a5805237ccd9b3f115aa8cd4e8e53eda1d6
Mitchell Young <youngmit@umich.edu>
2013-04-01 15:47:11 -0400

    some work on fixing the coarse ray trace

--- src/Geom_Points.f90
+++ src/Geom_Points.f90
@@ -453,9 +453,10 @@ MODULE Geom_Points
 !> firstPoint and before the last point in the list. If thisPoint
 !> returns to the calling routine associated then it was NOT inserted
 !> into the list successfully.
-    SUBROUTINE insert_LinkedListPointType(firstPoint,thisPoint)
+    SUBROUTINE insert_LinkedListPointType(firstPoint,thisPoint,force)
       CLASS(LinkedListPointType),TARGET,INTENT(INOUT) :: firstPoint
       TYPE(LinkedListPointType),POINTER,INTENT(INOUT) :: thisPoint
+      LOGICAL(SBK),INTENT(IN),OPTIONAL :: force
       
       LOGICAL(SBK) :: linsert
       REAL(SRK) :: d
@@ -474,23 +475,34 @@ MODULE Geom_Points
               !  (d .APPROXEQA. searchPoint2%sortval)) THEN
               IF((ABS(d-searchPoint1%sortval) <= 10._SRK*EPSREAL) .OR. &
                  (ABS(d-searchPoint2%sortval) <= 10._SRK*EPSREAL)) THEN
-                !Do not allow for points that are approximately equal to existing
-                !points in the list.
+                !Do not allow for points that are approximately equal to 
+                !existing points in the list. 
                 !
                 !Because the sort value is distance, "approximately equal to"
                 !should be use SQRT(dim)*EPSREAL instead of EPSREAL for the
                 !tolerance. We use a value of 2 since this caputes SQRT(3) and
                 !we don't really expect higher dimensions.
                 !
-                !However, in practice this was determined to be too small of a
-                !tolerance. It appears there are some cases where the approxeq is
-                !satisfied for 2 of the 3 dimensions and because the surface and
-                !line segment are nearly parallel the floating point error in the
-                !3rd dimension will be significantly larger.
+                !However, in practice this was determined to be too small of a 
+                !tolerance. It appears there are some cases where the approxeq 
+                !is satisfied for 2 of the 3 dimensions and because the surface 
+                !and line segment are nearly parallel the floating point error 
+                !in the 3rd dimension will be significantly larger. 
                 !
-                !A new factor of 10 was arbitrarily chosen and may need to be updated
-                !again if other degenerate cases are still encountered to not be
-                !be correctly identified. 
+                !A new factor of 10 was arbitrarily chosen and may need to be 
+                !updated again if other degenerate cases are still encountered 
+                !to not be be correctly identified.
+                IF(PRESENT(force))THEN
+                  IF(force) THEN
+                    ! We have forced the point to be inserted even if the point 
+                    ! appears to be a duplicate. Make them exactly the same 
+                    ! point.
+                    thisPoint%p%coord=searchPoint1%p%coord
+                    thisPoint%next => searchPoint2
+                    searchPoint1%next => thisPoint
+                    NULLIFY(thisPoint)
+                  ENDIF
+                ENDIF
                 linsert=.FALSE.
               ELSEIF(searchPoint1%sortval < d .AND. d < searchPoint2%sortval) THEN
                 thisPoint%next => searchPoint2
