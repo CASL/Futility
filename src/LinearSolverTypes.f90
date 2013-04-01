44277ddfc7a0de5ec233f532f21d24e2b88a96c1
Brendan Kochunas <bkochuna@umich.edu>
2013-04-01 14:04:24 -0400

    Added an IF block to solveGMRES to make sure that beta (norm of residual) is not exactly 0.0. If it is exactly 0.0, then the routine produces a NaN.

--- src/LinearSolverTypes.f90
+++ src/LinearSolverTypes.f90
@@ -1328,83 +1328,87 @@ MODULE LinearSolverTypes
       m=MIN(solver%nRestart,n)
       CALL u%init(pList)
       CALL pList%clear()
-      ALLOCATE(v(n,m+1))
-      ALLOCATE(R(m+1,m+1))
-      ALLOCATE(w(n))
-      ALLOCATE(c(m+1))
-      ALLOCATE(s(m+1))
-      ALLOCATE(g(m+1))
-      ALLOCATE(y(m+1))
-      v(:,:)=0._SRK
-      R(:,:)=0._SRK
-      w(:)=0._SRK
-      c(:)=0._SRK
-      s(:)=0._SRK
-      g(:)=0._SRK
-      y(:)=0._SRK
+      CALL solver%getResidual(u)
+      CALL LNorm(u%b,2,beta)
+      solver%iters=0
 
-      CALL solver%getResidual(u)
-      CALL LNorm(u%b,2,beta)
-      tol=solver%convTol*beta
-      
-      v(:,1)=-u%b/beta
-      h=beta
-      phibar=beta
-      !Iterate on solution
-      DO it=1,m
-        CALL BLAS_matvec(THISMATRIX=solver%A,X=v(:,it),BETA=0.0_SRK,Y=w)
-        h=BLAS_dot(n,w,1,v(:,1),1)
-        w=w-h*v(:,1)
-        t=h
-        DO k=2,it
-          h=BLAS_dot(n,w,1,v(:,k),1)
-          w=w-h*v(:,k)
-          R(k-1,it)=c(k-1)*t+s(k-1)*h
-          t=c(k-1)*h-s(k-1)*t
+      IF(beta > EPSILON(0.0_SRK)) THEN
+        ALLOCATE(v(n,m+1))
+        ALLOCATE(R(m+1,m+1))
+        ALLOCATE(w(n))
+        ALLOCATE(c(m+1))
+        ALLOCATE(s(m+1))
+        ALLOCATE(g(m+1))
+        ALLOCATE(y(m+1))
+        v(:,:)=0._SRK
+        R(:,:)=0._SRK
+        w(:)=0._SRK
+        c(:)=0._SRK
+        s(:)=0._SRK
+        g(:)=0._SRK
+        y(:)=0._SRK
+        tol=solver%convTol*beta
+        
+        v(:,1)=-u%b/beta
+        h=beta
+        phibar=beta
+        !Iterate on solution
+        DO it=1,m
+          CALL BLAS_matvec(THISMATRIX=solver%A,X=v(:,it),BETA=0.0_SRK,Y=w)
+          h=BLAS_dot(n,w,1,v(:,1),1)
+          w=w-h*v(:,1)
+          t=h
+          DO k=2,it
+            h=BLAS_dot(n,w,1,v(:,k),1)
+            w=w-h*v(:,k)
+            R(k-1,it)=c(k-1)*t+s(k-1)*h
+            t=c(k-1)*h-s(k-1)*t
+          ENDDO
+          CALL LNorm(w,2,h)
+          !WRITE(*,*) "h = ", h
+          IF(h > 0.0_SRK) THEN
+            v(:,it+1)=w/h
+          ELSE
+            v(:,it+1)=0.0_SRK*w
+          ENDIF
+          !Set up next Given's rotation
+          IF(t >= 0.0_SRK) THEN
+            temp=SQRT(t*t+h*h)
+          ELSE
+            temp=-SQRT(t*t+h*h)
+          ENDIF
+          c(it)=t/temp
+          s(it)=h/temp
+          R(it,it)=temp
+          g(it)=c(it)*phibar
+          phibar=-s(it)*phibar
+          IF(ABS(phibar) <= tol) EXIT
         ENDDO
-        CALL LNorm(w,2,h)
-        !WRITE(*,*) "h = ", h
-        IF(h > 0.0_SRK) THEN
-          v(:,it+1)=w/h
-        ELSE
-          v(:,it+1)=0.0_SRK*w
-        ENDIF
-        !Set up next Given's rotation
-        IF(t >= 0.0_SRK) THEN
-          temp=SQRT(t*t+h*h)
-        ELSE
-          temp=-SQRT(t*t+h*h)
-        ENDIF
-        c(it)=t/temp
-        s(it)=h/temp
-        R(it,it)=temp
-        g(it)=c(it)*phibar
-        phibar=-s(it)*phibar
-        IF(ABS(phibar) <= tol) EXIT
-      ENDDO
-      
-      DO j=it,1,-1
-        temp=0.0_SRK
-        DO k=j+1,it
-          temp=temp+R(j,k)*y(k)
+        
+        DO j=it,1,-1
+          temp=0.0_SRK
+          DO k=j+1,it
+            temp=temp+R(j,k)*y(k)
+          ENDDO
+          y(j)=(g(j)-temp)/R(j,j)
         ENDDO
-        y(j)=(g(j)-temp)/R(j,j)
-      ENDDO
       
-      CALL BLAS_matvec(v(:,1:it),y(1:it),0.0_SRK,u%b)
-      CALL BLAS_axpy(u,solver%x)
-      CALL solver%getResidual(u)
-      CALL LNorm(u%b,2,beta)
-      solver%iters=it
+        CALL BLAS_matvec(v(:,1:it),y(1:it),0.0_SRK,u%b)
+        CALL BLAS_axpy(u,solver%x)
+        CALL solver%getResidual(u)
+        CALL LNorm(u%b,2,beta)
+        IF(it == m+1) it=m
+        solver%iters=it
+        
+        DEALLOCATE(v)
+        DEALLOCATE(R)
+        DEALLOCATE(w)
+        DEALLOCATE(c)
+        DEALLOCATE(s)
+        DEALLOCATE(g)
+        DEALLOCATE(y)
+      ENDIF
       solver%info=0
-      
-      DEALLOCATE(v)
-      DEALLOCATE(R)
-      DEALLOCATE(w)
-      DEALLOCATE(c)
-      DEALLOCATE(s)
-      DEALLOCATE(g)
-      DEALLOCATE(y)
       CALL u%clear()
     ENDSUBROUTINE solveGMRES
 !
