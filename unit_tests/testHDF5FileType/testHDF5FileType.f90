9698b96d237dd93a802cf18f51176f913fb03a04
Benjamin Collins <bscollin@umich.edu>
2013-04-30 16:24:42 -0400

    Updated HDF5 test to be more stable and portable

--- unit_tests/testHDF5FileType/testHDF5FileType.f90
+++ unit_tests/testHDF5FileType/testHDF5FileType.f90
@@ -230,100 +230,229 @@ PROGRAM testHDF5
       TYPE(StringType) :: testC0
       CHARACTER(LEN=80),ALLOCATABLE :: sets(:)
       INTEGER(SIK) :: i,j,k
-      CHARACTER(LEN=8667) :: readcheck, readtest
       LOGICAL(SBK) :: checkread
 
-      CALL h5%init('readtest.h5','READ')
+!  Set up reference solutions (Copied from write routine)
+      REAL(SDK),ALLOCATABLE :: refD1(:),refD2(:,:),refD3(:,:,:)
+      REAL(SSK),ALLOCATABLE :: refS1(:),refS2(:,:),refS3(:,:,:)
+      LOGICAL(SBK),ALLOCATABLE :: refB1(:),refB2(:,:),refB3(:,:,:)
+      INTEGER(SLK),ALLOCATABLE :: refL1(:),refL2(:,:),refL3(:,:,:)
+      INTEGER(SNK),ALLOCATABLE :: refN1(:),refN2(:,:),refN3(:,:,:)
+      REAL(SDK) :: refD0
+      REAL(SSK) :: refS0
+      INTEGER(SLK) :: refL0
+      INTEGER(SNK) :: refN0
+      LOGICAL(SBK) :: refB0
+      TYPE(StringType) :: refC0
+      TYPE(StringType),ALLOCATABLE :: refC1(:),refC2(:,:),refC3(:,:,:)
+      CHARACTER(LEN=12) :: helper_string
 
-      CALL h5%ls('groupR',sets)
-      WRITE(readtest,*) TRIM(ADJUSTL(sets(1)))
-      DO i=2,SIZE(sets)
-        WRITE(readtest,*) TRIM(readtest)//sets(i)
-      ENDDO
+      ALLOCATE(refD1(10))
+      ALLOCATE(refD2(4,5))
+      ALLOCATE(refD3(3,4,5))
+      ALLOCATE(refS1(10))
+      ALLOCATE(refS2(4,5))
+      ALLOCATE(refS3(3,4,5))
+      ALLOCATE(refB1(6))
+      ALLOCATE(refB2(6,5))
+      ALLOCATE(refB3(6,5,2))
+      ALLOCATE(refL1(10))
+      ALLOCATE(refL2(4,5))
+      ALLOCATE(refL3(3,4,5))
+      ALLOCATE(refN1(10))
+      ALLOCATE(refN2(4,5))
+      ALLOCATE(refN3(3,4,5))
+      ALLOCATE(refC1(3))
+      ALLOCATE(refC2(2,3))
+      ALLOCATE(refC3(3,4,5))
 
-      ! Read a dataset (real-1)
-      CALL h5%fread('groupR->memD0',testD0)
-      CALL h5%fread('groupR->memD1',testD1)
-      CALL h5%fread('groupR->memD2',testD2)
-      CALL h5%fread('groupR->memD3',testD3)
-      CALL h5%fread('groupR->memS0',testS0)
-      CALL h5%fread('groupR->memS1',testS1)
-      CALL h5%fread('groupR->memS2',testS2)
-      CALL h5%fread('groupR->memS3',testS3)
-      CALL h5%fread('groupI->memN0',testN0)
-      CALL h5%fread('groupI->memN1',testN1)
-      CALL h5%fread('groupI->memN2',testN2)
-      CALL h5%fread('groupI->memN3',testN3)
-      CALL h5%fread('groupI->memL0',testL0)
-      CALL h5%fread('groupI->memL1',testL1)
-      CALL h5%fread('groupI->memL2',testL2)
-      CALL h5%fread('groupI->memL3',testL3)
-      CALL h5%fread('groupB->memB0',testB0)
-      CALL h5%fread('groupB->memB1',testB1)
-      CALL h5%fread('groupB->memB2',testB2)
-      CALL h5%fread('groupB->memB3',testB3)
-      CALL h5%fread('groupC->memC0',testC0)
-      CALL h5%fread('groupC->memC1',testC1)
-      CALL h5%fread('groupC->memC2',testC2)
-      CALL h5%fread('groupC->memC3',testC3)
-      
-      WRITE(readtest,*) TRIM(readtest),'testD0:'; WRITE(readtest,FMT=*)TRIM(readtest),testD0
-      WRITE(readtest,*) TRIM(readtest),'testD1:'; WRITE(readtest,FMT=*)TRIM(readtest),testD1
-      WRITE(readtest,*) TRIM(readtest),'testD2:'; WRITE(readtest,FMT=*)TRIM(readtest),testD2
-      WRITE(readtest,*) TRIM(readtest),'testD3:'; WRITE(readtest,FMT=*)TRIM(readtest),testD3
-      WRITE(readtest,*) TRIM(readtest),'testS0:'; WRITE(readtest,FMT=*)TRIM(readtest),testS0
-      WRITE(readtest,*) TRIM(readtest),'testS1:'; WRITE(readtest,FMT=*)TRIM(readtest),testS1
-      WRITE(readtest,*) TRIM(readtest),'testS2:'; WRITE(readtest,FMT=*)TRIM(readtest),testS2
-      WRITE(readtest,*) TRIM(readtest),'testS3:'; WRITE(readtest,FMT=*)TRIM(readtest),testS3
-      WRITE(readtest,*) TRIM(readtest),'testN0:'; WRITE(readtest,FMT=*)TRIM(readtest),testN0
-      WRITE(readtest,*) TRIM(readtest),'testN1:'; WRITE(readtest,FMT=*)TRIM(readtest),testN1
-      WRITE(readtest,*) TRIM(readtest),'testN2:'; WRITE(readtest,FMT=*)TRIM(readtest),testN2
-      WRITE(readtest,*) TRIM(readtest),'testN3:'; WRITE(readtest,FMT=*)TRIM(readtest),testN3
-      WRITE(readtest,*) TRIM(readtest),'testL0:'; WRITE(readtest,FMT=*)TRIM(readtest),testL0
-      WRITE(readtest,*) TRIM(readtest),'testL1:'; WRITE(readtest,FMT=*)TRIM(readtest),testL1
-      WRITE(readtest,*) TRIM(readtest),'testL2:'; WRITE(readtest,FMT=*)TRIM(readtest),testL2
-      WRITE(readtest,*) TRIM(readtest),'testL3:'; WRITE(readtest,FMT=*)TRIM(readtest),testL3
-      WRITE(readtest,*) TRIM(readtest),'testB0:'; WRITE(readtest,FMT=*)TRIM(readtest),testB0
-      WRITE(readtest,*) TRIM(readtest),'testB1:'; WRITE(readtest,FMT=*)TRIM(readtest),testB1
-      WRITE(readtest,*) TRIM(readtest),'testB2:'; WRITE(readtest,FMT=*)TRIM(readtest),testB2
-      WRITE(readtest,*) TRIM(readtest),'testB3:'; WRITE(readtest,FMT=*)TRIM(readtest),testB3
-      WRITE(readtest,*) TRIM(readtest),'testC0:'; WRITE(1,FMT=*)TRIM(readtest)//TRIM(testC0)
-      WRITE(readtest,*) TRIM(readtest),'testC1:'; 
-      DO i=1,SIZE(testC1)
-        WRITE(readtest,*)TRIM(readtest),CHAR(testC1(i))
-      ENDDO
-      WRITE(readtest,*) TRIM(readtest),'testC2:';
-      DO i=1,SIZE(testC2,1)
-        DO j=1,SIZE(testC2,2)
-          WRITE(readtest,*)TRIM(readtest),CHAR(testC2(i,j))
-        ENDDO
-      ENDDO
-      WRITE(readtest,*) TRIM(readtest),'testC3:';
-      DO i=1,SIZE(testC3,1)
-        DO j=1,SIZE(testC3,2)
-          DO k=1,SIZE(testC3,3)
-            WRITE(readtest,*)TRIM(readtest),CHAR(testC3(i,j,k))
+      refD0=42.123456789_SDK
+      refD1=[(i*1.0000000001_SDK,i=1,SIZE(refD1))]
+      refD2=RESHAPE([(i*1.0000000001_SDK,i=1,SIZE(refD2))],SHAPE(refD2))
+      refD3=RESHAPE([(i*1.0000000001_SDK,i=1,SIZE(refD3))],SHAPE(refD3))
+      refS0=42.000_SSK
+      refS1=[(i,i=1,SIZE(refS1))]
+      refS2=RESHAPE([(i,i=1,SIZE(refS2))],SHAPE(refS2))
+      refS3=RESHAPE([(i,i=1,SIZE(refS3))],SHAPE(refS3))
+      refL0=123456789123456789_SLK
+      refL1=[(i+1000000000,i=1,SIZE(refL1))]
+      refL2=RESHAPE([(i+1000000000,i=1,SIZE(refL2))],SHAPE(refL2))
+      refL3=RESHAPE([(i+1000000000,i=1,SIZE(refL3))],SHAPE(refL3))
+      refN0=42_SNK
+      refN1=[(i,i=1,SIZE(refN1))]
+      refN2=RESHAPE([(i,i=1,SIZE(refN2))],SHAPE(refN2))
+      refN3=RESHAPE([(i,i=1,SIZE(refN3))],SHAPE(refN3))
+      refB0=.FALSE.
+      refB1(:)=.TRUE.; refB2(:,:)=.TRUE.; refB3(:,:,:)=.TRUE.
+      DO i=1,SIZE(refB1),2
+        refB1(i)=.FALSE.
+        DO j=1,SIZE(refB2,DIM=2),3
+          refB2(i,j)=.FALSE.
+          DO k=1,SIZE(refB3,DIM=3)
+            refB3(i,j,k)=.FALSE.
           ENDDO
         ENDDO
       ENDDO
+      refC0='Rank-0 (Not-an-array) String Test'
+      DO i=1,SIZE(refC1)
+        WRITE(helper_string,FMT="(a7,i0)") 'String ',i
+        refC1(i)=helper_string
+      ENDDO
+      DO i=1,SIZE(refC2,1)
+        DO j=1,SIZE(refC2,2)
+          WRITE(helper_string,FMT="(a7,i0,a1,i0)") 'String ',i,',',j
+          refC2(i,j)=helper_string
+        ENDDO
+      ENDDO
+      refC2(1,1)='String 1,1';refC2(1,2)='String 1,2';refC2(2,1)='String 2,1'
+      refC2(2,2)='String 2,2';
+      DO i=1,SIZE(refC3,1)
+        DO j=1,SIZE(refC3,2)
+          DO k=1,SIZE(refC3,3)
+            WRITE(helper_string,FMT="(a7,i0,a1,i0,a1,i0)") 'String ',i,&
+                    &",",j,",",k
+            refC3(i,j,k)=helper_string
+          ENDDO
+        ENDDO
+      ENDDO
+      
+!  Begin test
+      CALL h5%init('readtest.h5','READ')
+
+      CALL h5%ls('groupR',sets)
+!      WRITE(readtest,*) TRIM(ADJUSTL(sets(1)))
+!      DO i=2,SIZE(sets)
+!        WRITE(readtest,*) TRIM(readtest)//sets(i)
+!      ENDDO
+
+      SET_PREFIX("hdf5%read()")
+      ! Read a dataset (real-1)
+      CALL h5%fread('groupR->memD0',testD0)
+      ASSERT(testD0==refD0,'D0 Failure')
+      CALL h5%fread('groupR->memD1',testD1)
+      ASSERT(ALL(testD1==refD1),'D1 Failure')
+      CALL h5%fread('groupR->memD2',testD2)
+      ASSERT(ALL(testD2==refD2),'D2 Failure')
+      CALL h5%fread('groupR->memD3',testD3)
+      ASSERT(ALL(testD3==refD3),'D3 Failure')
+      CALL h5%fread('groupR->memS0',testS0)
+      ASSERT(testS0==refS0,'S0 Failure')
+      CALL h5%fread('groupR->memS1',testS1)
+      ASSERT(ALL(testS1==refS1),'S1 Failure')
+      CALL h5%fread('groupR->memS2',testS2)
+      ASSERT(ALL(testS2==refS2),'S2 Failure')
+      CALL h5%fread('groupR->memS3',testS3)
+      ASSERT(ALL(testS3==refS3),'S3 Failure')
+      CALL h5%fread('groupI->memN0',testN0)
+      ASSERT(testN0==refN0,'N0 Failure')
+      CALL h5%fread('groupI->memN1',testN1)
+      ASSERT(ALL(testN1==refN1),'N1 Failure')
+      CALL h5%fread('groupI->memN2',testN2)
+      ASSERT(ALL(testN2==refN2),'N2 Failure')
+      CALL h5%fread('groupI->memN3',testN3)
+      ASSERT(ALL(testN3==refN3),'N3 Failure')
+      CALL h5%fread('groupI->memL0',testL0)
+      ASSERT(testL0==refL0,'L0 Failure')
+      CALL h5%fread('groupI->memL1',testL1)
+      ASSERT(ALL(testL1==refL1),'L1 Failure')
+      CALL h5%fread('groupI->memL2',testL2)
+      ASSERT(ALL(testL2==refL2),'L2 Failure')
+      CALL h5%fread('groupI->memL3',testL3)
+      ASSERT(ALL(testL3==refL3),'L3 Failure')
+      CALL h5%fread('groupB->memB0',testB0)
+      ASSERT(testB0.EQV.refB0,'B0 Failure')
+      CALL h5%fread('groupB->memB1',testB1)
+      ASSERT(ALL(testB1.EQV.refB1),'B1 Failure')
+      CALL h5%fread('groupB->memB2',testB2)
+      ASSERT(ALL(testB2.EQV.refB2),'B2 Failure')
+      CALL h5%fread('groupB->memB3',testB3)
+      ASSERT(ALL(testB3.EQV.refB3),'B3 Failure')
+      CALL h5%fread('groupC->memC0',testC0)
+      ASSERT(testC0==refC0,'C0 Failure')
+      CALL h5%fread('groupC->memC1',testC1)
+      checkread=.TRUE.
+      DO i=1,SIZE(refC1)
+        IF(testC1(i)/=refC1(i)) checkread=.FALSE.
+      ENDDO
+      ASSERT(checkread,'C1 Failure')
+      CALL h5%fread('groupC->memC2',testC2)
+      checkread=.TRUE.
+      DO i=1,SIZE(refC2,1)
+        DO j=1,SIZE(refC2,2)
+          IF(testC2(i,j)/=refC2(i,j)) checkread=.FALSE.
+        ENDDO
+      ENDDO
+      ASSERT(checkread,'C2 Failure')
+      CALL h5%fread('groupC->memC3',testC3)
+      checkread=.TRUE.
+      DO i=1,SIZE(refC3,1)
+        DO j=1,SIZE(refC3,2)
+          DO k=1,SIZE(refC3,3)
+            IF(testC3(i,j,k)/=refC3(i,j,k)) checkread=.FALSE.
+          ENDDO
+        ENDDO
+      ENDDO
+      ASSERT(checkread,'C3 Failure')
+      
+!      WRITE(readtest,*) TRIM(readtest),'testD0:'; WRITE(readtest,FMT=*)TRIM(readtest),testD0
+!      WRITE(readtest,*) TRIM(readtest),'testD1:'; WRITE(readtest,FMT=*)TRIM(readtest),testD1
+!      WRITE(readtest,*) TRIM(readtest),'testD2:'; WRITE(readtest,FMT=*)TRIM(readtest),testD2
+!      WRITE(readtest,*) TRIM(readtest),'testD3:'; WRITE(readtest,FMT=*)TRIM(readtest),testD3
+!      WRITE(readtest,*) TRIM(readtest),'testS0:'; WRITE(readtest,FMT=*)TRIM(readtest),testS0
+!      WRITE(readtest,*) TRIM(readtest),'testS1:'; WRITE(readtest,FMT=*)TRIM(readtest),testS1
+!      WRITE(readtest,*) TRIM(readtest),'testS2:'; WRITE(readtest,FMT=*)TRIM(readtest),testS2
+!      WRITE(readtest,*) TRIM(readtest),'testS3:'; WRITE(readtest,FMT=*)TRIM(readtest),testS3
+!      WRITE(readtest,*) TRIM(readtest),'testN0:'; WRITE(readtest,FMT=*)TRIM(readtest),testN0
+!      WRITE(readtest,*) TRIM(readtest),'testN1:'; WRITE(readtest,FMT=*)TRIM(readtest),testN1
+!      WRITE(readtest,*) TRIM(readtest),'testN2:'; WRITE(readtest,FMT=*)TRIM(readtest),testN2
+!      WRITE(readtest,*) TRIM(readtest),'testN3:'; WRITE(readtest,FMT=*)TRIM(readtest),testN3
+!      WRITE(readtest,*) TRIM(readtest),'testL0:'; WRITE(readtest,FMT=*)TRIM(readtest),testL0
+!      WRITE(readtest,*) TRIM(readtest),'testL1:'; WRITE(readtest,FMT=*)TRIM(readtest),testL1
+!      WRITE(readtest,*) TRIM(readtest),'testL2:'; WRITE(readtest,FMT=*)TRIM(readtest),testL2
+!      WRITE(readtest,*) TRIM(readtest),'testL3:'; WRITE(readtest,FMT=*)TRIM(readtest),testL3
+!      WRITE(readtest,*) TRIM(readtest),'testB0:'; WRITE(readtest,FMT=*)TRIM(readtest),testB0
+!      WRITE(readtest,*) TRIM(readtest),'testB1:'; WRITE(readtest,FMT=*)TRIM(readtest),testB1
+!      WRITE(readtest,*) TRIM(readtest),'testB2:'; WRITE(readtest,FMT=*)TRIM(readtest),testB2
+!      WRITE(readtest,*) TRIM(readtest),'testB3:'; WRITE(readtest,FMT=*)TRIM(readtest),testB3
+!      WRITE(readtest,*) TRIM(readtest),'testC0:'; WRITE(1,FMT=*)TRIM(readtest)//TRIM(testC0)
+!      WRITE(readtest,*) TRIM(readtest),'testC1:'; 
+!      DO i=1,SIZE(testC1)
+!        WRITE(readtest,*)TRIM(readtest),CHAR(testC1(i))
+!      ENDDO
+!      WRITE(readtest,*) TRIM(readtest),'testC2:';
+!      DO i=1,SIZE(testC2,1)
+!        DO j=1,SIZE(testC2,2)
+!          WRITE(readtest,*)TRIM(readtest),CHAR(testC2(i,j))
+!        ENDDO
+!      ENDDO
+!      WRITE(readtest,*) TRIM(readtest),'testC3:';
+!      DO i=1,SIZE(testC3,1)
+!        DO j=1,SIZE(testC3,2)
+!          DO k=1,SIZE(testC3,3)
+!            WRITE(readtest,*)TRIM(readtest),CHAR(testC3(i,j,k))
+!          ENDDO
+!        ENDDO
+!      ENDDO
 
       DEALLOCATE(testD1,testD2,testD3,testS1,testS2,testS3,testL1,testL2, &
             testL3,testB1,testB2,testB3,testC1,testC2,testC3,testN1,testN2, &
-            testN3)
+            testN3,refD1,refD2,refD3,refS1,refS2,refS3,refL1,refL2, &
+            refL3,refB1,refB2,refB3,refC1,refC2,refC3,refN1,refN2, &
+            refN3)
 
       CALL h5%clear()
       ASSERT(.NOT.h5%isinit, 'HDF5 object not properly cleared!')
       
-      CALL testHDF5_read_check(readcheck)
-      checkread=.TRUE.
-      DO i=1,LEN(readtest)
-        IF (readtest(i:i) /= readcheck(i:i)) THEN
-          checkread=.FALSE.
-          WRITE(*,*) i, readtest(i:i), readcheck(i:i)
-        ENDIF
-      ENDDO
-      
-      ASSERT(checkread, 'HDF5 file not properly read!')
+ !     CALL testHDF5_read_check(readcheck)
+ !     checkread=.TRUE.
+ !     DO i=1,LEN(readtest)
+ !       IF (readtest(i:i) /= readcheck(i:i)) THEN
+ !         checkread=.FALSE.
+ !         WRITE(*,*) i, readtest(i:i), readcheck(i:i)
+ !       ENDIF
+ !     ENDDO
+ !     
+ !     ASSERT(checkread, 'HDF5 file not properly read!')
       
     ENDSUBROUTINE testHDF5FileTypeRead
 !
