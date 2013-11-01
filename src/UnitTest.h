79c4c3ed2418d43a4ada39077551fc46fe4ef4aa
Benjamin Collins <bscollin@umich.edu>
2013-11-01 13:27:33 -0400

    Adding Copyright banner to the top of the the UnitTest header

--- src/UnitTest.h
+++ src/UnitTest.h
@@ -1,3 +1,21 @@
+!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+!                              Copyright (C) 2012                              !
+!                   The Regents of the University of Michigan                  !
+!              MPACT Development Group and Prof. Thomas J. Downar              !
+!                             All rights reserved.                             !
+!                                                                              !
+! Copyright is reserved to the University of Michigan for purposes of          !
+! controlled dissemination, commercialization through formal licensing, or     !
+! other disposition. The University of Michigan nor any of their employees,    !
+! makes any warranty, express or implied, or assumes any liability or          !
+! responsibility for the accuracy, completeness, or usefulness of any          !
+! information, apparatus, product, or process disclosed, or represents that    !
+! its use would not infringe privately owned rights. Reference herein to any   !
+! specific commercial products, process, or service by trade name, trademark,  !
+! manufacturer, or otherwise, does not necessarily constitute or imply its     !
+! endorsement, recommendation, or favoring by the University of Michigan.      !
+!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+
 #define UTEST_FAIL_CODE 666
 
 #define CREATE_TEST(name)  CALL UTest_Start(name)
