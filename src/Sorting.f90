1140989eb93775dc0c0c4b2040779511c1354d22
Dan Jabaay <djabaay@umich.edu>
2013-11-01 14:42:56 -0400

    Adding a new module to house some simple sorting routines.

---
+++ src/Sorting.f90
@@ -0,0 +1,35 @@
+!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+!                              Copyright (C) 2012                              !
+!                   The Regents of the University of Michigan                  !
+!              MPACT Development Group and Prof. Thomas J. Downar              !
+!                             All rights reserved.                             !
+!                                                                              !
+! Copyright is reserved to the University of Michigan for purposes of          !
+! controlled dissemination, commercialization through formal licensing, or     !
+! other disposition. The University of Michigan nor any of their employees,    !
+! makes any warranty, express or implied, or assumes any liability or          !
+! responsibility for the accuracy, completeness, or usefulness of any          !
+! information, apparatus, product, or process disclosed, or represents that    !
+! its use would not infringe privately owned rights. Reference herein to any   !
+! specific commercial products, process, or service by trade name, trademark,  !
+! manufacturer, or otherwise, does not necessarily constitute or imply its     !
+! endorsement, recommendation, or favoring by the University of Michigan.      !
+!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+!> @brief A Fortran 2003 module implementing some basic sorting algorithms.
+!> 
+!> @par Module Dependencies
+!>  - @ref IntrType "IntrType": @copybrief IntrType
+!>
+!> @author Dan Jabaay
+!>    @date 11/1/2013
+!>
+!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+MODULE Sorting
+  
+  USE IntrType
+  IMPLICIT NONE
+  
+  !> Avogadro's Number
+  REAL(SRK),PARAMETER :: Na=6.02214129000E+23_SRK !16 digits
+ 
+ENDMODULE Sorting
