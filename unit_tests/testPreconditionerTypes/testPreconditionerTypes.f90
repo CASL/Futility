75ce57242b7865773800e26ced3a3ffdf324780b
Shane Stimpson <sgstim@umich.edu>
2013-11-20 02:39:11 -0500

    modified some output in the preconditioner unit test

--- unit_tests/testPreconditionerTypes/testPreconditionerTypes.f90
+++ unit_tests/testPreconditionerTypes/testPreconditionerTypes.f90
@@ -287,34 +287,40 @@ PROGRAM testMatrixTypes
         ! Check L
         ! Check U
         SELECTTYPE(A => testLU%A); TYPE IS(PETScMatrixType)
+          WRITE(667,*) "A: "
           DO row=1,A%n
             WRITE(667,'(I4)',ADVANCE='NO') row
             DO col=1,A%n
               CALL A%get(row,col,tmpval)
-              WRITE(667,'(E15.5)',ADVANCE='NO') tmpval
+              WRITE(667,'(E20.10)',ADVANCE='NO') tmpval
             ENDDO   
             WRITE(667,*)            
           ENDDO
+          WRITE(667,*)  
         ENDSELECT
         SELECTTYPE(A => testLU%L); TYPE IS(PETScMatrixType)
+          WRITE(667,*) "L: "
           DO row=1,A%n
             WRITE(667,'(I4)',ADVANCE='NO') row
             DO col=1,A%n
               CALL A%get(row,col,tmpval)
-              WRITE(667,'(E15.5)',ADVANCE='NO') tmpval
+              WRITE(667,'(E20.10)',ADVANCE='NO') tmpval
             ENDDO   
             WRITE(667,*)            
           ENDDO
+          WRITE(667,*)  
         ENDSELECT
         SELECTTYPE(A => testLU%U); TYPE IS(PETScMatrixType)
+          WRITE(667,*) "U: "
           DO row=1,A%n
             WRITE(667,'(I4)',ADVANCE='NO') row
             DO col=1,A%n
               CALL A%get(row,col,tmpval)
-              WRITE(667,'(E15.5)',ADVANCE='NO') tmpval
+              WRITE(667,'(E20.10)',ADVANCE='NO') tmpval
             ENDDO   
             WRITE(667,*)            
           ENDDO
+          WRITE(667,*)  
         ENDSELECT
         
         ! Check %apply
