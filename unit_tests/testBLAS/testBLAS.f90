31770be512cc8d6b20e47ddae831db6ff91e23fc
Aaron Graham <aarograh@umich.edu>
2013-11-27 11:25:23 -0500

    Fixed some ASSERT statements

--- unit_tests/testBLAS/testBLAS.f90
+++ unit_tests/testBLAS/testBLAS.f90
@@ -2243,7 +2243,7 @@ PROGRAM testBLAS
       ia2(1:5)=(/1,2,4,7,11/)
       sx(15:18)=(/1.0000000_SSK,-0.5000000_SSK,-0.1666666666666666_SSK,-0.083333333333333333_SSK/)
       CALL BLAS_matvec('L','N','N',sx(1:10),ia2(1:5),ja2(1:10),sx(11:14))
-      ASSERT(ALL(sx(11:14) .APPROXEQA. sx(15:18)),'CALL BLAS_matvec(''L'',''N'',''N'',sa,ia,ja,sx')
+      ASSERT(ALL(sx(11:14) .APPROXEQA. sx(15:18)),'CALL BLAS_matvec(''L'',''N'',''N'',sa,ia,ja,sx)')
       FINFO() 'Calculated:',sx(11:14),' Solution:',sx(15:18)
       
       sx(1:10)=(/1.0_SSK,2.0_SSK,3.0_SSK,4.0_SSK,2.0_SSK,3.0_SSK,4.0_SSK,3.0_SSK,4.0_SSK,4.0_SSK/)
@@ -2251,7 +2251,7 @@ PROGRAM testBLAS
       ja2(1:10)=(/1,2,3,4,2,3,4,3,4,4/)
       ia2(1:5)=(/1,5,8,10,11/)
       CALL BLAS_matvec('U','T','N',sx(1:10),ia2(1:5),ja2(1:10),sx(11:14))
-      ASSERT(ALL(sx(11:14) .APPROXEQA. sx(15:18)),'CALL BLAS_matvec(''U'',''T'',''N'',sa,ia,ja,sx')
+      ASSERT(ALL(sx(11:14) .APPROXEQA. sx(15:18)),'CALL BLAS_matvec(''U'',''T'',''N'',sa,ia,ja,sx)')
       FINFO() 'Calculated:',sx(11:14),' Solution:',sx(15:18)
       
       sx(1:10)=(/1.0_SSK,2.0_SSK,3.0_SSK,4.0_SSK,2.0_SSK,3.0_SSK,4.0_SSK,3.0_SSK,4.0_SSK,4.0_SSK/)
@@ -2260,7 +2260,7 @@ PROGRAM testBLAS
       ia2(1:5)=(/1,5,8,10,11/)
       sx(15:18)=(/0.00000000_SSK,0.0000000_SSK,0.0000000_SSK,0.25000000_SSK/)
       CALL BLAS_matvec('U','N','N',sx(1:10),ia2(1:5),ja2(1:10),sx(11:14))
-      ASSERT(ALL(sx(11:14) .APPROXEQA. sx(15:18)),'CALL BLAS_matvec(''U'',''N'',''N'',sa,ia,ja,sx')
+      ASSERT(ALL(sx(11:14) .APPROXEQA. sx(15:18)),'CALL BLAS_matvec(''U'',''N'',''N'',sa,ia,ja,sx)')
       FINFO() 'Calculated:',sx(11:14),' Solution:',sx(15:18)
       
       sx(1:10)=(/1.0_SSK,2.0_SSK,2.0_SSK,3.0_SSK,3.0_SSK,3.0_SSK,4.0_SSK,4.0_SSK,4.0_SSK,4.0_SSK/)
@@ -2268,7 +2268,7 @@ PROGRAM testBLAS
       ja2(1:10)=(/1,1,2,1,2,3,1,2,3,4/)
       ia2(1:5)=(/1,2,4,7,11/)
       CALL BLAS_matvec('L','T','N',sx(1:10),ia2(1:5),ja2(1:10),sx(11:14))
-      ASSERT(ALL(sx(11:14) .APPROXEQA. sx(15:18)),'CALL BLAS_matvec(''L'',''T'',''N'',sa,ia,ja,sx')
+      ASSERT(ALL(sx(11:14) .APPROXEQA. sx(15:18)),'CALL BLAS_matvec(''L'',''T'',''N'',sa,ia,ja,sx)')
       FINFO() 'Calculated:',sx(11:14),' Solution:',sx(15:18)
       
       !dtrsv_all_sparse
