9572f3b9bf6239b98b91d5495c91749bdb72e6e9
Aaron Graham <aarograh@umich.edu>
2013-11-28 09:49:42 -0500

    Minor modifications to BLAS_matvec calls in apply_LU_PreCondType

--- src/PreconditionerTypes.f90
+++ src/PreconditionerTypes.f90
@@ -389,8 +389,8 @@ MODULE PreconditionerTypes
             SELECTTYPE(L => PC%L)
               CLASS IS(SparseMatrixType)
                 SELECTTYPE(U => PC%U); TYPE IS(SparseMatrixType)
-                  CALL BLAS_matvec('L','N','T',L%a,L%ia,L%ja,v%b)
-                  CALL BLAS_matvec('U','N','N',U%a,U%ia,U%ja,v%b)
+                  CALL BLAS_matvec(THISMATRIX=L,X=v,Y=v,UPLO='L',DIAG='T',TRANS='N')
+                  CALL BLAS_matvec(THISMATRIX=U,X=v,Y=v,UPLO='U',DIAG='N',TRANS='N')
                 ENDSELECT
               CLASS DEFAULT
                 CALL ePreCondType%raiseError('Incorrect input to '//modName//'::'//myName// &
