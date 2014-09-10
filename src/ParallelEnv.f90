e629f996a99b415403eacdaa902b7ea3da649536
Aaron Graham <aarograh@umich.edu>
2014-09-10 13:22:01 -0400

    Squashed commit to merge branch ctf into master.

--- src/ParallelEnv.f90
+++ src/ParallelEnv.f90
@@ -162,8 +162,24 @@ MODULE ParallelEnv
       !> @copybrief ParallelEnv::bcast_SLK0_MPI_Env_type
       !> @copydetails ParallelEnv::bcast_SLK0_MPI_Env_type
       PROCEDURE,PASS,PRIVATE :: bcast_SLK0_MPI_Env_type
+      !> @copybrief ParallelEnv::bcast_SSK2_MPI_Env_type
+      !> @copydetails ParallelEnv::bcast_SSK2_MPI_Env_type
+      PROCEDURE,PASS,PRIVATE :: bcast_SSK2_MPI_Env_type
+      !> @copybrief ParallelEnv::bcast_SDK2_MPI_Env_type
+      !> @copydetails ParallelEnv::bcast_SDK2_MPI_Env_type
+      PROCEDURE,PASS,PRIVATE :: bcast_SDK2_MPI_Env_type
+      !> @copybrief ParallelEnv::bcast_SSK4_MPI_Env_type
+      !> @copydetails ParallelEnv::bcast_SSK4_MPI_Env_type
+      PROCEDURE,PASS,PRIVATE :: bcast_SSK4_MPI_Env_type
+      !> @copybrief ParallelEnv::bcast_SDK4_MPI_Env_type
+      !> @copydetails ParallelEnv::bcast_SDK4_MPI_Env_type
+      PROCEDURE,PASS,PRIVATE :: bcast_SDK4_MPI_Env_type
       !>
-      GENERIC :: bcast => bcast_SLK0_MPI_Env_type
+      GENERIC :: bcast => bcast_SLK0_MPI_Env_type, &
+                          bcast_SSK2_MPI_Env_type, &
+                          bcast_SDK2_MPI_Env_type, &
+                          bcast_SSK4_MPI_Env_type, &
+                          bcast_SDK4_MPI_Env_type
       !> @copybrief ParallelEnv::allReduceR_MPI_Env_type
       !> @copydetails  ParallelEnv::allReduceR_MPI_Env_type
       PROCEDURE,PASS :: allReduce => allReduceR_MPI_Env_type
@@ -712,6 +728,86 @@ MODULE ParallelEnv
           ' - Invalid rank!')
       ENDIF
     ENDSUBROUTINE bcast_SLK0_MPI_Env_type
+!
+!-------------------------------------------------------------------------------
+!> @brief
+    SUBROUTINE bcast_SSK2_MPI_Env_type(myPE,buf,root)
+      CHARACTER(LEN=*),PARAMETER :: myName='bcast_SSK2_MPI_Env_type'
+      CLASS(MPI_EnvType),INTENT(INOUT) :: myPE
+      REAL(SSK),INTENT(IN) :: buf(:,:)
+      INTEGER(SIK),INTENT(IN),OPTIONAL :: root
+      INTEGER(SIK) :: rank
+      rank=0
+      IF(PRESENT(root)) rank=root
+      IF(0 <= rank .AND. rank <= myPE%nproc-1) THEN
+#ifdef HAVE_MPI
+        CALL MPI_Bcast(buf,SIZE(buf),MPI_REAL4,rank,myPE%comm,mpierr)
+#endif
+      ELSE
+        CALL eParEnv%raiseError(modName//'::'//myName// &
+          ' - Invalid rank!')
+      ENDIF
+    ENDSUBROUTINE bcast_SSK2_MPI_Env_type
+!
+!-------------------------------------------------------------------------------
+!> @brief
+    SUBROUTINE bcast_SDK2_MPI_Env_type(myPE,buf,root)
+      CHARACTER(LEN=*),PARAMETER :: myName='bcast_SDK2_MPI_Env_type'
+      CLASS(MPI_EnvType),INTENT(INOUT) :: myPE
+      REAL(SDK),INTENT(IN) :: buf(:,:)
+      INTEGER(SIK),INTENT(IN),OPTIONAL :: root
+      INTEGER(SIK) :: rank
+      rank=0
+      IF(PRESENT(root)) rank=root
+      IF(0 <= rank .AND. rank <= myPE%nproc-1) THEN
+#ifdef HAVE_MPI
+        CALL MPI_Bcast(buf,SIZE(buf),MPI_REAL8,rank,myPE%comm,mpierr)
+#endif
+      ELSE
+        CALL eParEnv%raiseError(modName//'::'//myName// &
+          ' - Invalid rank!')
+      ENDIF
+    ENDSUBROUTINE bcast_SDK2_MPI_Env_type
+!
+!-------------------------------------------------------------------------------
+!> @brief
+    SUBROUTINE bcast_SSK4_MPI_Env_type(myPE,buf,root)
+      CHARACTER(LEN=*),PARAMETER :: myName='bcast_SSK4_MPI_Env_type'
+      CLASS(MPI_EnvType),INTENT(INOUT) :: myPE
+      REAL(SSK),INTENT(IN) :: buf(:,:,:,:)
+      INTEGER(SIK),INTENT(IN),OPTIONAL :: root
+      INTEGER(SIK) :: rank
+      rank=0
+      IF(PRESENT(root)) rank=root
+      IF(0 <= rank .AND. rank <= myPE%nproc-1) THEN
+#ifdef HAVE_MPI
+        CALL MPI_Bcast(buf,SIZE(buf),MPI_REAL4,rank,myPE%comm,mpierr)
+#endif
+      ELSE
+        CALL eParEnv%raiseError(modName//'::'//myName// &
+          ' - Invalid rank!')
+      ENDIF
+    ENDSUBROUTINE bcast_SSK4_MPI_Env_type
+!
+!-------------------------------------------------------------------------------
+!> @brief
+    SUBROUTINE bcast_SDK4_MPI_Env_type(myPE,buf,root)
+      CHARACTER(LEN=*),PARAMETER :: myName='bcast_SDK4_MPI_Env_type'
+      CLASS(MPI_EnvType),INTENT(INOUT) :: myPE
+      REAL(SDK),INTENT(IN) :: buf(:,:,:,:)
+      INTEGER(SIK),INTENT(IN),OPTIONAL :: root
+      INTEGER(SIK) :: rank
+      rank=0
+      IF(PRESENT(root)) rank=root
+      IF(0 <= rank .AND. rank <= myPE%nproc-1) THEN
+#ifdef HAVE_MPI
+        CALL MPI_Bcast(buf,SIZE(buf),MPI_REAL8,rank,myPE%comm,mpierr)
+#endif
+      ELSE
+        CALL eParEnv%raiseError(modName//'::'//myName// &
+          ' - Invalid rank!')
+      ENDIF
+    ENDSUBROUTINE bcast_SDK4_MPI_Env_type
 !
 !-------------------------------------------------------------------------------
 !> @brief Wrapper routine calls MPI_Allreduce and performs a sum of operation
