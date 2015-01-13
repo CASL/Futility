ea96ccfb1716ea6da488221cdca2473234d2b0a1
Dan Jabaay <djabaay@umich.edu>
2015-01-13 15:42:08 -0500

    Fixing Valgrind error.
    HDF5 test didn't clear the files when it needed to.

--- unit_tests/testHDF5FileType/testHDF5FileType.f90
+++ unit_tests/testHDF5FileType/testHDF5FileType.f90
@@ -667,6 +667,8 @@ PROGRAM testHDF5
 
       CALL h5%e%setQuietMode(.FALSE.)
       CALL h5%e%setStopOnError(.TRUE.)
+      
+      CALL h5%clear(.TRUE.)
       !CALL h5%fdelete()
       !INQUIRE(FILE='writetest.h5',EXIST=exists)
       !ASSERT(.NOT.exists,'HDF5 object not properly deleted!')
@@ -929,6 +931,8 @@ PROGRAM testHDF5
       i=h5%ngrp('groupR')
       ASSERT(i == 10,'ngrp_HDF5FileType')
       FINFO() i
+      !Clear variables
+      CALL h5%clear(.TRUE.)
      
     ENDSUBROUTINE testCompress
 !
