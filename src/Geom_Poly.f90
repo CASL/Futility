4f97a1fb53067b3bcefcd9cf406a6633e22bc5d3
Dan Jabaay <djabaay@umich.edu>
2015-06-10 14:06:58 -0400

    WIP: Updating Geom_Poly for Andrew.
    Set and Clear routines implemented and tested.  Area tests are being
    computed correctly for test cases.
    
    Currently implementing %inside and %intersectLine functions.

--- src/Geom_Poly.f90
+++ src/Geom_Poly.f90
@@ -30,63 +30,27 @@
 !>  - @ref IntrType "IntrType": @copybrief IntrType
 !>  - @ref Geom_Points "Geom_Points": @copybrief Geom_Points
 !>  - @ref Geom_Line "Geom_Line": @copybrief Geom_Line
+!>  - @ref Geom_Line "Geom_Plane": @copybrief Geom_Plane
+!>  - @ref Geom_Line "Geom_CircCyl": @copybrief Geom_CircCyl
+!>  - @ref Geom_Line "Geom_Graph": @copybrief Geom_Graph
 !>
-!> @author Brendan Kochunas
+!> @author Brendan Kochunas, Dan Jabaay
 !>    @date 06/06/2015
 !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
 MODULE Geom_Poly
   USE IntrType
+  USE Allocs
   USE Constants_Conversion
   USE Geom_Points
   USE Geom_Line
   USE Geom_Plane
   USE Geom_CircCyl
-  USE Geom_Box
   USE Geom_Graph
   
   IMPLICIT NONE
   PRIVATE
   
-  !> @brief Type describing intersection between geometrical primitives
-  !>
-  !> Derived type to store an intersection between two surfaces/curves that
-  !> potentially bound a region of the mesh.
-  TYPE :: MesherIntersectType
-    !> x coordinate of the intersection point
-    REAL(SRK) :: x=0.0_SRK
-    !> y coordinate of the intersection point
-    REAL(SRK) :: y=0.0_SRK
-    !> does it have a circle?
-    LOGICAL :: circ=.FALSE.
-    !> circle radius. The intersection can only involve one circle. This is a
-    !> limitation which shouldnt cause any issues since out circles are always
-    !> concentric.
-    REAL(SRK) :: r=0.0_SRK
-    !> If a value has been assigned to the intersection
-    LOGICAL :: isSet=.FALSE.
-!!
-!!List of type bound procedure for the object
-!    CONTAINS
-!      !> Placeholder for the set routine
-!      PROCEDURE,PASS :: set => set_MesherIntersectType
-!      !> Placeholder for the clear routine
-!      PROCEDURE,PASS :: clear => clear_MesherIntersectType
-  ENDTYPE MesherIntersectType
-
-  !> @brief Type used for storing a vertex in PolygonType
-  !>
-  !> Derived type to store the vertex in a polygon. The following derived type
-  !> is a linked list of this type which stores all of the vertices in a whole
-  !> polygon.
-  TYPE :: PolygonVertexType
-    !> An element in the linked list
-    TYPE(MesherIntersectType) :: point
-    !> Pointer to the next element in the linked list
-    TYPE(PolygonVertexType),POINTER :: nextElem => NULL()
-    !> this is the angle of the point w.r.t the centroid of the FSR. It is used
-    !> to sort the points and ensure that the polygon is ordered CCW.
-    REAL(SRK) :: theta
-  ENDTYPE PolygonVertexType
+  PUBLIC :: PolygonType
 
   !> @brief Type used to describe a polygon.
   !>
@@ -97,43 +61,41 @@ MODULE Geom_Poly
   !> of the polygon. The finalize method points the last element to the first.
   !> This makes the area calculation a lot cleaner.
   TYPE :: PolygonType
-    !> The entry element of the list
-    TYPE(PolygonVertexType),POINTER :: firstElem => NULL()
-    !> Bounding box. ultimately all points must fall in this region
-    TYPE(ABBoxType) :: box
-    !> Minimum angular bound of the FSR of interest
-    REAL(SRK) :: azMin=0.0_SRK
-    !> Maximum angular bound of the FSR of interest
-    REAL(SRK) :: azMax=0.0_SRK
-    !> Minimum radial bound of the FSR of interest
-    REAL(SRK) :: rMin=0.0_SRK
-    !> Maximum radial bound of the FSR of interest
-    REAL(SRK) :: rMax=0.0_SRK
-    !> X coordinate of the reference point for angle-sorting.
-    REAL(SRK) :: x=0.0_SRK
-    !> Y coordinate of the reference point for angle-sorting.
-    REAL(SRK) :: y=0.0_SRK
-    !> The number of elements in the linked list of vertices
-    INTEGER(SIK) :: nElem=0 
     !> Bool for if the object is initialized
     LOGICAL :: isInit=.FALSE.
-!!
-!!List of type bound procedure for the object
-!    CONTAINS
-!      !> Placeholder for set routine
-!      PROCEDURE,PASS :: set    => set_PolygonType
-!      !> Placeholder for clear routine
-!      PROCEDURE,PASS :: clear  => clear_PolygonType
+    !> Area of the polygon
+    REAL(SRK) :: area=0.0_SRK
+    !> The number of elements in the linked list of vertices
+    INTEGER(SIK) :: nVert=0 
+    !> The number of elements in the linked list of edges
+    INTEGER(SIK) :: nEdge=0 
+    !> The list of vertices
+    TYPE(PointType),ALLOCATABLE :: vert(:)
+    !> The list of edges by the vert index (SIZE 2,nvert)
+    INTEGER(SIK),ALLOCATABLE :: edge(:,:)
+    !> The list of quadratic edges (circles) (SIZE 3,nEdge)
+    REAL(SRK),ALLOCATABLE :: quadEdge(:,:)
+!
+!List of type bound procedure for the object
+    CONTAINS
+      !> @copybrief Geom_Poly::set_PolygonType
+      !> @copydetails Geom_Poly::set_PolygonType
+      PROCEDURE,PASS :: set => set_PolygonType
+      !> @copybrief Geom_Poly::clear_PolygonType
+      !> @copydetails Geom_Poly::clear_PolygonType
+      PROCEDURE,PASS :: clear => clear_PolygonType
+      !> @copybrief Geom_Poly::inside_PolygonType
+      !> @copydetails Geom_Poly::inside_PolygonType
+      PROCEDURE,PASS :: inside => inside_PolygonType
+      !> @copybrief Geom_Poly::intersect_PolygonType
+      !> @copydetails Geom_Poly::intersect_PolygonType
+      PROCEDURE,PASS :: intersectLine => intersectLine_PolygonType
 !      !> Placeholder for insertion routine
 !      PROCEDURE,PASS :: insert => insert_PolygonType
 !      !> Placeholder for the finilize routine (points last element at first)
 !      PROCEDURE,PASS :: finalize => finalize_PolygonType
 !      !> Placeholder for interior point routine
 !      PROCEDURE,PASS :: interior => interior_PolygonType
-!      !> Placeholder to the report routine
-!      PROCEDURE,PASS :: report => report_PolygonType
-!      !> Placeholder for the area calculation routine
-!      PROCEDURE,PASS :: area => area_PolygonType
   ENDTYPE PolygonType
     
   INTERFACE Polygonize
@@ -143,6 +105,159 @@ MODULE Geom_Poly
 !
 !===============================================================================
   CONTAINS
+!
+!-------------------------------------------------------------------------------
+!> @brief
+!> @param
+!>
+    SUBROUTINE set_PolygonType(thisPoly,thatGraph)
+      CLASS(PolygonType) :: thisPoly
+      TYPE(GraphType) :: thatGraph
+      
+      INTEGER(SIK) :: i,icurr,inextold,inext,nvert
+      REAL(SRK) :: a,r,h,R1,coeff
+      TYPE(PointType) :: point
+      TYPE(LineType) :: line
+      
+      !Check if thatGraph is closed (i.e. each vertex has only two neighbors)
+      !Pick the starting vertex, loop over all vertices until back to the original,
+      !  calculating angles for each set of three. Increment nElem.
+      IF(ALLOCATED(thatGraph%vertices)) THEN
+        !IF(thatGraph%isMinSet()) THEN
+          !Get the number of vertices and edges (equal, since it's a minimum cycle)
+          thisPoly%nVert=thatGraph%nVert()
+          thisPoly%nEdge=thatGraph%nVert()
+          ALLOCATE(thisPoly%vert(thisPoly%nVert))
+          ALLOCATE(thisPoly%edge(2,thisPoly%nEdge))
+          CALL dmallocA(thisPoly%quadEdge,3,thisPoly%nEdge)
+          thisPoly%quadEdge=0.0_SRK
+          !Setup initial points
+          
+          !Need to use the edgeMatrix to find the neighboring points, can't assume ordered.
+          CALL thisPoly%vert(1)%init(DIM=2,X=thatGraph%vertices(1,1), &
+            Y=thatGraph%vertices(2,1))
+          thisPoly%edge(1,1)=1
+          !Loop over all vertices in the graphtype in CW ordering.
+          inext=thatGraph%getCWMostVert(0,1)
+          inextold=1
+          icurr=1
+          DO i=2,thisPoly%nVert
+            !Get the next vertex using the neighbor call
+            !Using the CCW-th point, the area calc will be positive.  CW would be negative.
+            CALL thisPoly%vert(i)%init(DIM=2,X=thatGraph%vertices(1,inext), &
+              Y=thatGraph%vertices(2,inext))
+            thisPoly%area=thisPoly%area+thisPoly%vert(i-1)%coord(1)* &
+              thisPoly%vert(i)%coord(2)-thisPoly%vert(i-1)%coord(2)* &
+              thisPoly%vert(i)%coord(1)
+            !Set the edge
+            thisPoly%edge(2,i-1)=i
+            thisPoly%edge(1,i)=i
+            !Check if it's a quadratic edge and assign
+            IF(thatGraph%quadEdges(3,icurr,inext) > 0.0_SRK) &
+              thisPoly%quadEdge(:,i-1)=thatGraph%quadEdges(:,icurr,inext)
+            inextold=inext
+            inext=thatGraph%getCWMostVert(icurr,inext)
+            icurr=inextold
+          ENDDO
+          !Set last edge
+          thisPoly%edge(2,thisPoly%nVert)=1
+          thisPoly%area=thisPoly%area+thisPoly%vert(thisPoly%nVert)%coord(1)* &
+            thisPoly%vert(1)%coord(2)-thisPoly%vert(thisPoly%nVert)%coord(2)* &
+            thisPoly%vert(1)%coord(1)
+          !Check if it's a quadratic edge and assign
+          IF(thatGraph%quadEdges(3,1,icurr) > 0.0_SRK) &
+            thisPoly%quadEdge(:,thisPoly%nVert)=thatGraph%quadEdges(:,1,icurr)
+          !Last component of the area calc.
+          thisPoly%area=ABS(thisPoly%area*0.5_SRK)
+          
+          !Now do the chord area checks, because they're all positive values.
+          DO i=1,thisPoly%nEdge
+            coeff=1.0_SRK
+            IF(thisPoly%quadEdge(3,i) > 0.0_SRK) THEN
+              CALL point%init(DIM=2,X=thisPoly%quadEdge(1,i), &
+                Y=thisPoly%quadEdge(2,i))
+              !Setup line to test whether to add or subtract
+              CALL line%set(thisPoly%vert(thisPoly%edge(1,i)), &
+                thisPoly%vert(thisPoly%edge(2,i)))
+              !Modify area - taken from Wolfram's circular segment calculation.
+              R1=thisPoly%quadEdge(3,i)
+              !Calculate "a"
+              a=distance(thisPoly%vert(thisPoly%edge(1,i)), &
+                thisPoly%vert(thisPoly%edge(2,i)))
+              !Calculate "r"
+              r=0.5_SRK*SQRT(4.0_SRK*R1*R1-a*a)
+              !Calculate "h"
+              h=R1-r
+              !Calculate Chord Sector area (Arc area - triangle area)
+              IF(line%pointIsLeft(point)) coeff=-1.0_SRK
+              thisPoly%area=thisPoly%area+coeff*(R1*R1*ACOS((R1-h)/R1)- &
+                (R1-h)*SQRT(2*R1*h-h*h))
+            ENDIF
+          ENDDO
+          thisPoly%isinit=.TRUE.
+        !ENDIF
+      ENDIF
+      
+    ENDSUBROUTINE set_PolygonType
+!
+!-------------------------------------------------------------------------------
+!> @brief
+!> @param
+!>
+    SUBROUTINE clear_PolygonType(thisPorygon)
+      CLASS(PolygonType),INTENT(INOUT) :: thisPorygon
+      INTEGER(SIK) :: i
+    
+      !Clear the vertex data
+      DO i=thisPorygon%nVert,1,-1
+        CALL thisPorygon%vert(i)%clear()
+      ENDDO
+      IF(ALLOCATED(thisPorygon%vert)) DEALLOCATE(thisPorygon%vert)
+      IF(ALLOCATED(thisPorygon%edge)) DEALLOCATE(thisPorygon%edge)
+      CALL demallocA(thisPorygon%quadEdge)
+      thisPorygon%nVert=0
+      thisPorygon%nEdge=0
+      thisPorygon%area=0.0_SRK
+      thisPorygon%isinit=.FALSE.
+    ENDSUBROUTINE clear_PolygonType
+!
+!-------------------------------------------------------------------------------
+!> @brief 
+!> @param
+!>
+    ELEMENTAL FUNCTION inside_PolygonType(thisPolygon,point) RESULT(bool)
+      CLASS(PolygonType),INTENT(IN) :: thisPolygon
+      TYPE(PointType),INTENT(IN) :: point
+      LOGICAL(SBK) :: bool
+      INTEGER(SIK) :: i
+      
+      !Traverse through all the edges, test the point to see if it is always
+      !to the left (or right) of the lines since we will be ordering them.
+      !Only applies to convex polygons.
+      bool=.FALSE.
+      IF(thisPolygon%isinit) THEN
+        DO i=1,thisPolygon%nEdge
+          !Create line
+          !
+        ENDDO
+      ENDIF
+    ENDFUNCTION inside_PolygonType
+!
+!-------------------------------------------------------------------------------
+!> @brief
+!> @param
+!>
+    ELEMENTAL SUBROUTINE intersectLine_PolygonType(thisPolygon,line,p1,p2)
+      CLASS(PolygonType),INTENT(IN) :: thisPolygon
+      TYPE(LineType),INTENT(IN) :: line
+      TYPE(PointType),INTENT(INOUT) :: p1,p2
+      
+      !Test the line with all the edges, make sure they aren't co-linear
+      !Get the intersections from the line_line_intersect routine.
+      !Should only need two points for convex geometry polygons.
+      !If one of the edges is quadratic, calculate the point from the coefficients
+      
+    ENDSUBROUTINE intersectLine_PolygonType
 !
 !-------------------------------------------------------------------------------
 !> @brief
