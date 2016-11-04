e42de7746ce2b1f83abd6da2795b35e003c2f87d
Benjamin Collins <collinsbs@ornl.gov>
2016-11-04 11:58:14 -0400

    Fixing duplicated divide by n issue when scaling

--- src/EigenvalueSolverTypes.f90
+++ src/EigenvalueSolverTypes.f90
@@ -837,7 +837,7 @@ MODULE EigenvalueSolverTypes
       CALL solver%x_scale%set(0.0_SRK)
       CALL BLAS_matvec(THISMATRIX=solver%A,X=solver%x,Y=solver%x_scale)
       factor=BLAS_asum(solver%x_scale)/REAL(solver%n,SRK)
-      CALL BLAS_scal(THISVECTOR=solver%X,A=REAL(solver%n,SRK)/factor)
+      CALL BLAS_scal(THISVECTOR=solver%X,A=1.0_SRK/factor)
 #endif
     ENDSUBROUTINE solve_EigenvalueSolverType_Anasazi
 !
