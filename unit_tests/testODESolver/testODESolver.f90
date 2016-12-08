55fcb0030119585d478eaa248214cf77871ccef4
Benjamin Collins <collinsbs@ornl.gov>
2016-12-08 18:06:08 -0500

    Extending unit tests to look at order of convergence
    CASL Ticket # - 4579

--- unit_tests/testODESolver/testODESolver.f90
+++ unit_tests/testODESolver/testODESolver.f90
@@ -11,14 +11,24 @@ MODULE testODEInterface
   USE IntrType
   USE VectorTypes
   USE ODESolverTypes
-  TYPE,EXTENDS(ODESolverInterface_Base) :: ODESolverInterface_Test
+  TYPE,EXTENDS(ODESolverInterface_Base) :: ODESolverInterface_TestLinear
     CONTAINS
       !> Deferred routine for initializing the ode solver
-      PROCEDURE,PASS :: eval => eval
-  ENDTYPE ODESolverInterface_Test
+      PROCEDURE,PASS :: eval => eval_linear
+  ENDTYPE ODESolverInterface_TestLinear
+  TYPE,EXTENDS(ODESolverInterface_Base) :: ODESolverInterface_TestExponential
+    CONTAINS
+      !> Deferred routine for initializing the ode solver
+      PROCEDURE,PASS :: eval => eval_exp
+  ENDTYPE ODESolverInterface_TestExponential
+  TYPE,EXTENDS(ODESolverInterface_Base) :: ODESolverInterface_TestNonLinear
+    CONTAINS
+      !> Deferred routine for initializing the ode solver
+      PROCEDURE,PASS :: eval => eval_nonlinear
+  ENDTYPE ODESolverInterface_TestNonLinear
   CONTAINS
-    SUBROUTINE eval(self,t,y,ydot)
-      CLASS(ODESolverInterface_Test),INTENT(INOUT) :: self
+    SUBROUTINE eval_linear(self,t,y,ydot)
+      CLASS(ODESolverInterface_TestLinear),INTENT(INOUT) :: self
       REAL(SRK),INTENT(IN) :: t
       CLASS(VectorType),INTENT(INOUT) :: y
       CLASS(VectorType),INTENT(INOUT) :: ydot
@@ -32,7 +42,38 @@ MODULE testODEInterface
       CALL ydot%set(1,3.2_SRK)
       CALL ydot%set(2,0.0_SRK)
       CALL ydot%set(3,-0.3_SRK)
-    ENDSUBROUTINE eval
+    ENDSUBROUTINE eval_linear
+    !
+    SUBROUTINE eval_exp(self,t,y,ydot)
+      CLASS(ODESolverInterface_TestExponential),INTENT(INOUT) :: self
+      REAL(SRK),INTENT(IN) :: t
+      CLASS(VectorType),INTENT(INOUT) :: y
+      CLASS(VectorType),INTENT(INOUT) :: ydot
+
+      REAL(SRK) :: tmp(3)
+      CALL ydot%set(0.0_SRK)
+      CALL y%get(tmp)
+      !CALL ydot%set(1,3.2_SRK*tmp(1)-2.4_SRK*tmp(2)+5.0_SRK*tmp(3))
+      !CALL ydot%set(2,tmp(1)-10.0_SRK*tmp(3))
+      !CALL ydot%set(3,-0.3_SRK*SUM(tmp))
+      CALL ydot%set(1,3.2_SRK*tmp(1))
+      CALL ydot%set(2,tmp(1))
+      CALL ydot%set(3,-0.3_SRK*tmp(3))
+    ENDSUBROUTINE eval_exp
+    !
+    SUBROUTINE eval_nonlinear(self,t,y,ydot)
+      CLASS(ODESolverInterface_TestNonLinear),INTENT(INOUT) :: self
+      REAL(SRK),INTENT(IN) :: t
+      CLASS(VectorType),INTENT(INOUT) :: y
+      CLASS(VectorType),INTENT(INOUT) :: ydot
+
+      REAL(SRK) :: tmp(3)
+      CALL ydot%set(0.0_SRK)
+      CALL y%get(tmp)
+      CALL ydot%set(1,1.0E-3*(tmp(3)-tmp(3)*tmp(2)*tmp(2)))
+      CALL ydot%set(2,1.0E-3_SRK*(1.053-tmp(1)))
+      CALL ydot%set(3,1.0E-3_SRK*(3.089-tmp(2)))
+    ENDSUBROUTINE eval_nonlinear
 ENDMODULE testODEInterface
 
 PROGRAM testODESolver
@@ -82,7 +123,7 @@ PROGRAM testODESolver
   CREATE_TEST('Test ODE Solvers')
 
   ALLOCATE(ODESolverType_Native :: testODE)
-  ALLOCATE(ODESolverInterface_Test:: f)
+  ALLOCATE(ODESolverInterface_TestLinear :: f)
 
   REGISTER_SUBTEST('testInit_Native',testInit_Native)
   REGISTER_SUBTEST('testStep_Native_theta=0.0',testStep_Native)
@@ -94,6 +135,19 @@ PROGRAM testODESolver
     testODE%theta=1.0_SRK
   ENDSELECT
   REGISTER_SUBTEST('testStep_Native_theta=1.0',testStep_Native)
+
+  DEALLOCATE(f)
+  ALLOCATE(ODESolverInterface_TestExponential :: f)
+  testODE%f=>f
+
+  REGISTER_SUBTEST('testStep_Native_exp',testStep_Native_exp)
+
+  DEALLOCATE(f)
+  ALLOCATE(ODESolverInterface_TestNonLinear :: f)
+  testODE%f=>f
+
+  REGISTER_SUBTEST('testStep_Native_nonlinear',testStep_Native_nonlinear)
+
   REGISTER_SUBTEST('testClear_Native',testClear_Native)
   FINALIZE_TEST()
 
@@ -172,6 +226,49 @@ CONTAINS
     ENDSUBROUTINE testStep_Native
 !
 !-------------------------------------------------------------------------------
+    SUBROUTINE testStep_Native_exp()
+      REAL(SRK) :: ref(3)
+
+      ref(1)=3.0_SRK*EXP(3.2_SRK*3.5_SRK)
+      ref(2)=3.0_SRK*(EXP(3.2_SRK*3.5_SRK)-1.0_SRK)/3.2_SRK+3.0_SRK
+      ref(3)=3.0_SRK*EXP(-0.3_SRK*3.5_SRK)
+      SELECTTYPE(testODE); TYPE IS(ODESolverType_Native)
+        testODE%theta=0.0_SRK
+      ENDSELECT
+      CALL testOrderConv(1.0_SRK,0.1_SRK,ref,'theta=0.0')
+      SELECTTYPE(testODE); TYPE IS(ODESolverType_Native)
+        testODE%theta=0.5_SRK
+      ENDSELECT
+      CALL testOrderConv(2.0_SRK,0.1_SRK,ref,'theta=0.5')
+      SELECTTYPE(testODE); TYPE IS(ODESolverType_Native)
+        testODE%theta=1.0_SRK
+      ENDSELECT
+      CALL testOrderConv(1.0_SRK,0.1_SRK,ref,'theta=1.0')
+
+    ENDSUBROUTINE testStep_Native_exp
+!
+!-------------------------------------------------------------------------------
+    SUBROUTINE testStep_Native_nonlinear()
+      REAL(SRK) :: ref(3)
+
+      !This reference was generated with 1e-6 substep size
+      ref=(/2.9162069562508526_SRK,2.9933322575306889_SRK,3.0003232540442979_SRK/)
+      SELECTTYPE(testODE); TYPE IS(ODESolverType_Native)
+        testODE%theta=0.0_SRK
+      ENDSELECT
+      CALL testOrderConv(1.0_SRK,0.1_SRK,ref,'theta=0.0')
+      SELECTTYPE(testODE); TYPE IS(ODESolverType_Native)
+        testODE%theta=0.5_SRK
+      ENDSELECT
+      CALL testOrderConv(2.0_SRK,0.1_SRK,ref,'theta=0.5')
+      SELECTTYPE(testODE); TYPE IS(ODESolverType_Native)
+        testODE%theta=1.0_SRK
+      ENDSELECT
+      CALL testOrderConv(1.0_SRK,0.1_SRK,ref,'theta=1.0')
+
+    ENDSUBROUTINE testStep_Native_nonlinear
+!
+!-------------------------------------------------------------------------------
     SUBROUTINE testClear_Native()
       CALL testODE%clear()
       SELECTTYPE(testODE); TYPE IS(ODESolverType_Native)
@@ -188,20 +285,49 @@ CONTAINS
     ENDSUBROUTINE testClear_Native
 !
 !-------------------------------------------------------------------------------
+    SUBROUTINE testOrderConv(exp_order,tol,ref,tag)
+      REAL(SRK),INTENT(IN) :: exp_order
+      REAL(SRK),INTENT(IN) :: tol
+      REAL(SRK),INTENT(IN) :: ref(:)
+      CHARACTER(LEN=*),INTENT(IN) :: tag
+
+      INTEGER(SIK) :: i,j
+      REAL(SRK) :: substep, calc_order
+      REAL(SRK) :: tmp(3,4)
+      TYPE(ParamType) :: tmpPL
+      TYPE(RealVectorType) :: y0, yf
+      LOGICAL(SBK) :: bool
+
+      CALL tmpPL%add('VectorType->n',3_SIK)
+      CALL y0%init(tmpPL)
+      CALL yf%init(tmpPL)
+
+      substep=1.0E-2_SRK
+      DO i=1,4
+        CALL y0%set(3.0_SRK)
+        CALL yf%set(0.0_SRK)
+        SELECTTYPE(testODE); TYPE IS(ODESolverType_Native)
+          testODE%substep_size=substep
+        ENDSELECT
+
+        CALL testODE%step(0.0_SRK,y0,3.5_SRK,yf)
+
+        CALL yf%get(tmp(:,i))
+        tmp(:,i)=tmp(:,i)-ref
+        substep=substep*0.1_SRK
+      ENDDO
+
+      DO i=1,3 ! reduction order
+        DO j=1,3 ! solution index
+          bool=(ABS(exp_order-LOG10(tmp(j,i)/tmp(j,i+1)))<=tol .OR. ABS(tmp(j,i))<=1.0E-7)
+          ASSERT(bool,'Order Convergence: '//TRIM(tag))
+          FINFO() tmp(j,:)
+          FINFO() i,j,exp_order,LOG10(tmp(j,i)/tmp(j,i+1)),ABS(exp_order-LOG(tmp(j,i)/tmp(j,i+1)))
+        ENDDO
+      ENDDO
+
+    ENDSUBROUTINE testOrderConv
+!
+!-------------------------------------------------------------------------------
 !
 ENDPROGRAM testODESolver
-
-  SUBROUTINE f(t,y,ydot)
-    USE IntrType
-    USE VectorTypes
-    REAL(SRK),INTENT(IN) :: t
-    CLASS(VectorType),INTENT(IN) :: y
-    CLASS(VectorType),INTENT(INOUT) :: ydot
-WRITE(*,*) "in f"
-WRITE(*,*) t
-WRITE(*,*) y%isInit
-WRITE(*,*) ydot%isInit
-    CALL ydot%set(0.0_SRK)
-    CALL ydot%set(1,3.2_SRK)
-    CALL ydot%set(3,-0.3_SRK)
-  ENDSUBROUTINE f
