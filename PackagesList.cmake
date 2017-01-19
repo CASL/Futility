f3d606551aedfdc848ee6c0955e74cadc45ffbc4
Benjamin Collins <collinsbs@ornl.gov>
2017-01-19 15:27:21 -0500

    Adding basic TriBITS infrastructure
    CASL Ticket #4684

---
+++ PackagesList.cmake
@@ -0,0 +1,14 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+#                          Futility Development Group                          !
+#                             All rights reserved.                             !
+#                                                                              !
+# Futility is a jointly-maintained, open-source project between the University !
+# of Michigan and Oak Ridge National Laboratory.  The copyright and license    !
+# can be found in LICENSE.txt in the head directory of this repository.        !
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+
+SET(Futility_PACKAGES_AND_DIRS_AND_CLASSIFICATIONS
+  futility      .               SS
+  )
+
+TRIBITS_ALLOW_MISSING_EXTERNAL_PACKAGES()
