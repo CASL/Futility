08dc3a09a4de60bf48aedd5befc822a8afac985e
Benjamin Collins <collinsbs@ornl.gov>
2017-01-21 23:48:31 -0500

    Fixing comments for c, cpp, h, and hpp files to only be 80 characters

--- src/trilinos_interfaces/trilinos_mat_vec.hpp
+++ src/trilinos_interfaces/trilinos_mat_vec.hpp
@@ -1,11 +1,11 @@
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
-//                          Futility Development Group                          !
-//                             All rights reserved.                             !
-//                                                                              !
-// Futility is a jointly-maintained, open-source project between the University !
-// of Michigan and Oak Ridge National Laboratory.  The copyright and license    !
-// can be found in LICENSE.txt in the head directory of this repository.        !
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+/                          Futility Development Group                          !
+/                             All rights reserved.                             !
+/                                                                              !
+/ Futility is a jointly-maintained, open-source project between the University !
+/ of Michigan and Oak Ridge National Laboratory.  The copyright and license    !
+/ can be found in LICENSE.txt in the head directory of this repository.        !
+/+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 #pragma once
 
 #include <iostream>
