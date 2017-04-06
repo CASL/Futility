05a8dcb8162128e6d3e3f4a4d2637d8320c85f6e
Benjamin Collins <collinsbs@ornl.gov>
2017-04-06 00:14:46 -0400

    Adding a common project file shared between MPACT and Futility and adding projectname

---
+++ ProjectName.cmake
@@ -0,0 +1,20 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+#                          Futility Development Group                          !
+#                             All rights reserved.                             !
+#                                                                              !
+# Futility is a jointly-maintained, open-source project between the University !
+# of Michigan and Oak Ridge National Laboratory.  The copyright and license    !
+# can be found in LICENSE.txt in the head directory of this repository.        !
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+
+SET(PROJECT_NAME Futility)
+
+INCLUDE(cmake/Project_Common.cmake)
+
+# Put in hard disables for excluded packages
+FOREACH(EXCLUDED_PACKAGE ${${PROJECT_NAME}_EXCLUDE_PACKAGES})
+  SET(${PROJECT_NAME}_ENABLE_${EXCLUDED_PACKAGE} OFF CACHE BOOL
+    "Disabled in Futility ProjectName.cmake"
+    ${FORCE_EXCLUDED_PACKAGE_DISABLE}
+    )
+ENDFOREACH()
