3737d35d3c3a00a7a6ef33f8c1f4b67880bf4200
Benjamin Collins <collinsbs@ornl.gov>
2017-05-18 23:33:14 -0400

    Adding unit tests for sundials interface

--- unit_tests/testODESolver/testODESolver.f90
+++ unit_tests/testODESolver/testODESolver.f90
@@ -167,11 +167,36 @@ PROGRAM testODESolver
   REGISTER_SUBTEST('testStep_Native_nonlinear',testStep_Native_nonlinear)
 
   REGISTER_SUBTEST('testClear_Native',testClear_Native)
+
+! Sundials tests
+  DEALLOCATE(testODE)
+  DEALLOCATE(f)
+  ALLOCATE(ODESolverType_Sundials :: testODE)
+  ALLOCATE(ODESolverInterface_TestLinear :: f)
+
+  CALL pList%set('ODESolverType->solver',BDF_METHOD)
+  CALL pList%set('ODESolverType->bdf_order',3_SIK)
+  CALL pList%set('ODESolverType->tolerance',1.0e-10_SRK)
+
+  REGISTER_SUBTEST('testInit_Sundials',testInit_Sundials)
+#ifdef FUTILITY_HAVE_SUNDIALS
+  REGISTER_SUBTEST('testStep_Sundials_Linear',testStep_Sundials_Linear)
+  DEALLOCATE(f)
+  ALLOCATE(ODESolverInterface_TestExponential :: f)
+  SUNDIALS_ODE_INTERFACE=>f
+  REGISTER_SUBTEST('testStep_Sundials_Exp',testStep_Sundials_Exp)
+  DEALLOCATE(f)
+  ALLOCATE(ODESolverInterface_TestNonLinear :: f)
+  SUNDIALS_ODE_INTERFACE=>f
+  REGISTER_SUBTEST('testStep_Sundials_NonLinear',testStep_Sundials_NonLinear)
+#endif
+  REGISTER_SUBTEST('testClear_Sundials',testClear_Sundials)
+
   FINALIZE_TEST()
 
   CALL pList%clear()
-
   DEALLOCATE(testODE)
+  DEALLOCATE(f)
 
 #ifdef FUTILITY_HAVE_PETSC
   CALL PetscFinalize(ierr)
@@ -345,6 +370,143 @@ CONTAINS
         ASSERT(.NOT. testODE%isInit,'%isInit')
       ENDSELECT
     ENDSUBROUTINE testClear_Native
+!
+!-------------------------------------------------------------------------------
+    SUBROUTINE testInit_Sundials()
+      INTEGER(SIK) :: errcnt
+      errcnt=eODESolverType%getCounter(EXCEPTION_ERROR)
+      SELECTTYPE(testODE); TYPE IS(ODESolverType_Sundials)
+        CALL testODE%init(pList,f)
+        ASSERT(testODE%n==3,'%n')
+        ASSERT(testODE%solverMethod==BDF_METHOD,'%solverMethod')
+        ASSERT(testODE%BDForder==3,'%BDForder')
+        ASSERT(testODE%tol==1.0E-10_SRK,'%tol')
+        ASSERT(ALLOCATED(testODE%ytmp),'%ytmp Allocated')
+#ifdef FUTILITY_HAVE_SUNDIALS
+        ASSERT(testODE%isInit,'%isInit')
+        ASSERT(ASSOCIATED(testODE%f),'%f')
+        ASSERT(ASSOCIATED(SUNDIALS_ODE_INTERFACE),'SUNDIALS_ODE_INTERFACE')
+#else
+        ASSERT(eODESolverType%getCounter(EXCEPTION_ERROR)-errcnt == 1,'ERROR')
+#endif
+      ENDSELECT
+
+    ENDSUBROUTINE testInit_Sundials
+!
+!-------------------------------------------------------------------------------
+    SUBROUTINE testStep_Sundials_Linear()
+      REAL(SRK) :: tmpval
+      TYPE(ParamType) :: tmpPL
+      TYPE(RealVectorType) :: y0, yf
+
+      CALL tmpPL%add('VectorType->n',3_SIK)
+      CALL y0%init(tmpPL)
+      CALL yf%init(tmpPL)
+
+      CALL y0%set(3.0_SRK)
+      CALL yf%set(0.0_SRK)
+
+      CALL testODE%step(0.0_SRK,y0,3.5_SRK,yf)
+
+      CALL yf%get(1,tmpval)
+      ASSERT(SOFTEQ(tmpval,14.2_SRK,1.0E-12_SRK),'yf(1)')
+      FINFO() tmpval
+      CALL yf%get(2,tmpval)
+      ASSERT(SOFTEQ(tmpval,3.00_SRK,1.0E-12_SRK),'yf(2)')
+      FINFO() tmpval
+      CALL yf%get(3,tmpval)
+      ASSERT(SOFTEQ(tmpval,1.95_SRK,1.0E-12_SRK),'yf(3)')
+      FINFO() tmpval
+
+      CALL testODE%step(0.0_SRK,y0,3.5_SRK,yf)
+
+      CALL yf%get(1,tmpval)
+      ASSERT(SOFTEQ(tmpval,14.2_SRK,1.0E-12_SRK),'yf(1)')
+      FINFO() tmpval
+      CALL yf%get(2,tmpval)
+      ASSERT(SOFTEQ(tmpval,3.00_SRK,1.0E-12_SRK),'yf(2)')
+      FINFO() tmpval
+      CALL yf%get(3,tmpval)
+      ASSERT(SOFTEQ(tmpval,1.95_SRK,1.0E-12_SRK),'yf(3)')
+      FINFO() tmpval
+    ENDSUBROUTINE testStep_Sundials_Linear
+!
+!-------------------------------------------------------------------------------
+    SUBROUTINE testStep_Sundials_Exp()
+      REAL(SRK) :: tmpval
+      TYPE(ParamType) :: tmpPL
+      TYPE(RealVectorType) :: y0, yf
+      REAL(SRK) :: ref(3)
+
+      ref(1)=3.0_SRK*EXP(3.2_SRK*3.5_SRK)
+      ref(2)=3.0_SRK*(EXP(3.2_SRK*3.5_SRK)-1.0_SRK)/3.2_SRK+3.0_SRK
+      ref(3)=3.0_SRK*EXP(-0.03_SRK*3.5_SRK)
+
+      CALL tmpPL%add('VectorType->n',3_SIK)
+      CALL y0%init(tmpPL)
+      CALL yf%init(tmpPL)
+
+      CALL y0%set(3.0_SRK)
+      CALL yf%set(0.0_SRK)
+
+      CALL testODE%step(0.0_SRK,y0,3.5_SRK,yf)
+
+      CALL yf%get(1,tmpval)
+      ASSERT(SOFTEQR(tmpval,ref(1),2.0E-8_SRK),'yf(1)')
+      FINFO() tmpval, ref(1), tmpval/ref(1)-1.0_SRK
+      CALL yf%get(2,tmpval)
+      ASSERT(SOFTEQR(tmpval,ref(2),2.0E-8_SRK),'yf(2)')
+      FINFO() tmpval, ref(2), tmpval/ref(2)-1.0_SRK
+      CALL yf%get(3,tmpval)
+      ASSERT(SOFTEQR(tmpval,ref(3),2.0E-8_SRK),'yf(3)')
+      FINFO() tmpval, ref(3), tmpval/ref(3)-1.0_SRK
+
+    ENDSUBROUTINE testStep_Sundials_Exp
+!
+!-------------------------------------------------------------------------------
+    SUBROUTINE testStep_Sundials_NonLinear()
+      REAL(SRK) :: tmpval
+      TYPE(ParamType) :: tmpPL
+      TYPE(RealVectorType) :: y0, yf
+      REAL(SRK) :: ref(3)
+
+      ref=(/2.9162069601728970_SRK,2.9933322574820909_SRK,3.0003232540397478_SRK/)
+
+      CALL tmpPL%add('VectorType->n',3_SIK)
+      CALL y0%init(tmpPL)
+      CALL yf%init(tmpPL)
+
+      CALL y0%set(3.0_SRK)
+      CALL yf%set(0.0_SRK)
+
+      CALL testODE%step(0.0_SRK,y0,3.5_SRK,yf)
+
+      CALL yf%get(1,tmpval)
+      ASSERT(SOFTEQR(tmpval,ref(1),2.0E-8_SRK),'yf(1)')
+      FINFO() tmpval, ref(1), tmpval/ref(1)-1.0_SRK
+      CALL yf%get(2,tmpval)
+      ASSERT(SOFTEQR(tmpval,ref(2),2.0E-8_SRK),'yf(2)')
+      FINFO() tmpval, ref(2), tmpval/ref(2)-1.0_SRK
+      CALL yf%get(3,tmpval)
+      ASSERT(SOFTEQR(tmpval,ref(3),2.0E-8_SRK),'yf(3)')
+      FINFO() tmpval, ref(3), tmpval/ref(3)-1.0_SRK
+
+    ENDSUBROUTINE testStep_Sundials_NonLinear
+!
+!-------------------------------------------------------------------------------
+    SUBROUTINE testClear_Sundials()
+      CALL testODE%clear()
+      SELECTTYPE(testODE); TYPE IS(ODESolverType_Sundials)
+        ASSERT(testODE%solverMethod==-1,'%solverMethod')
+        ASSERT(testODE%TPLType==-1,'%TPLType')
+        ASSERT(testODE%n==-1,'%n')
+        ASSERT(testODE%tol==1.0e-8_SRK,'%tol')
+        ASSERT(testODE%BDForder==5,'%BDForder')
+        ASSERT(.NOT. ALLOCATED(testODE%ytmp),'%ytmp NOT Allocated')
+        ASSERT(.NOT. ASSOCIATED(SUNDIALS_ODE_INTERFACE),'SUNDIALS_ODE_INTERFACE')
+        ASSERT(.NOT. testODE%isInit,'%isInit')
+      ENDSELECT
+    ENDSUBROUTINE testClear_Sundials
 !
 !-------------------------------------------------------------------------------
     SUBROUTINE testOrderConv(exp_order,tol,ref,tag)
