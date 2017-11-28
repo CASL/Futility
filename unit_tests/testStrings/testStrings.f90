4e0bb6d0eccbc4ac7d0655cb70b951cb4f5d6475
Dan Jabaay <djabaay@umich.edu>
2017-11-28 14:32:50 -0500

    Adding assignment overloading for reals to strings.
    Description:
    Since we could assign an integer to a string type, adding a similar
    overload for reals seemed useful.  Set up and tested in the same manner
    as the integers were.
    
    CASL Ticket # - N/A

--- unit_tests/testStrings/testStrings.f90
+++ unit_tests/testStrings/testStrings.f90
@@ -18,6 +18,8 @@ PROGRAM testStrings
   INTEGER(SIK) :: i
   INTEGER(SNK) :: testSNK,testSNKarray(2)
   INTEGER(SLK) :: testSLK,testSLKarray(2)
+  REAL(SSK) :: testSSK,testSSKarray(2)
+  REAL(SDK) :: testSDK,testSDKarray(2)
   CHARACTER(LEN=10) :: char10
   CHARACTER(LEN=20) :: char20
   TYPE(StringType) :: testString,testString2,testStringArray(10),testarray(2)
@@ -99,9 +101,9 @@ PROGRAM testStrings
 !
 !Test assigning a long integer to string
   COMPONENT_TEST('Assignment 64-bit Integer to String')
-  testSLK=1_SLK
+  testSLK=1.01_SSK
   testString=testSLK
-  ASSERT(CHAR(testString) == '1','testString=testSLK : 1')
+  ASSERT(CHAR(testString) == '1','testString=testSLK : 1.01')
   testString=-HUGE(testSLK)
   ASSERT(CHAR(testString) == '-9223372036854775807','testString=testSLK : lowest')
   testString=HUGE(testSLK)
@@ -110,6 +112,36 @@ PROGRAM testStrings
   testarray=testSLKarray
   ASSERT(CHAR(testarray(1)) == '0','testString=testSLK : array(1)')
   ASSERT(CHAR(testarray(2)) == '5','testString=testSLK : array(2)')
+!
+!Test assigning a single real to string
+  COMPONENT_TEST('Assignment 32-bit Real to String')
+  testSSK=1.01_SSK
+  testString=testSSK
+  ASSERT(CHAR(testString) == '1.01000E+00','testString=testSSK : 1')
+  testString=-HUGE(testSSK)
+  ASSERT(CHAR(testString) == '-3.40282E+38','testString=testSSK : lowest')
+  testString=HUGE(testSSK)
+  ASSERT(CHAR(testString) == '3.40282E+38','testString=testSSK : highest')
+  testSSKarray=(/-2.0_SSK,50.0_SSK/)
+  testarray=testSSKarray
+  ASSERT(CHAR(testarray(1)) == '-2.00000E+00','testString=testSSK : array(1)')
+  ASSERT(CHAR(testarray(2)) == '5.00000E+01','testString=testSSK : array(2)')
+!
+!Test assigning a double real to string
+  COMPONENT_TEST('Assignment 64-bit Real to String')
+  testSDK=1.01_SDK
+  testString=testSDK
+  ASSERT(CHAR(testString) == '1.010000000000000E+00','testString=testSDK : 1.01')
+  !Since HUGE produces a triple digit exponent, it seems the 'E' disappears when
+  !writing.
+  testString=-HUGE(testSDK)
+  ASSERT(CHAR(testString) == '-1.797693134862316+308','testString=testSDK : lowest')
+  testString=HUGE(testSDK)
+  ASSERT(CHAR(testString) == '1.797693134862316+308','testString=testSDK : highest')
+  testSDKarray=(/-2.0_SDK,50.0_SDK/)
+  testarray=testSDKarray
+  ASSERT(CHAR(testarray(1)) == '-2.000000000000000E+00','testString=testSDK : array(1)')
+  ASSERT(CHAR(testarray(2)) == '5.000000000000000E+01','testString=testSDK : array(2)')
 !
 !Test assigning a char to a 1-D string array
   COMPONENT_TEST('Assignment char to 1-D String Array')
