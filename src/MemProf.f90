d8767e5dcd67f29cc13444b486dd398c930304f0
bscollin <collinsbs@ornl.gov>
2017-12-15 20:25:07 -0500

    Memprof update (#78)
    * Update to memprof to add better control
    
    Added ability to turn profiler edit on and off outside of preproc definition.  Now
    the preprocessor has to be on AND edits has to be on.  Also added ability to
    define a maximum delta memory that triggers an edit.
    
    * Add verbose output for memeory processor.
    
    Added the ability to do processor by processor output to files called memprof.####.dat
    where ### is the mpi rank.
    
    CASL Ticket #: 5244
    
    * Add banners and processor of maximum change
    
    Added a banner to normal and verbose memory profiling labeling what things are and the units.
    Also added the rank of the process that has the maximum change in memory.
    
    CASL Ticket #: 5244
    
    * Removing unused variable

--- src/MemProf.f90
+++ src/MemProf.f90
@@ -21,6 +21,7 @@ MODULE MemProf
   USE ISO_C_BINDING
   USE IntrType
   USE Strings
+  USE FileType_Fortran
   USE FileType_Log
   USE ExceptionHandler
   USE ParameterLists
@@ -41,6 +42,17 @@ MODULE MemProf
     REAL(SRK) :: mem_current=0.0_SRK
     !> Memory last time edit was called
     REAL(SRK) :: mem_old=0.0_SRK
+    !> Memory threshold
+    REAL(SRK) :: mem_threshold=0.0_SRK
+    !> logical to enable and disabled verbose edits
+    LOGICAL(SBK) :: verbose=.FALSE.
+    !> Verbose file to output to
+    TYPE(FortranFileType) :: verbose_output
+    !> logical to enable and disabled edits
+    LOGICAL(SBK) :: mem_edit=.TRUE.
+    !> Logical to print banner explaining what data is available
+    LOGICAL(SBK) :: print_banner=.TRUE.
+    !> log file
     TYPE(LogFileType),POINTER :: mylog => NULL()
     !> Parallel Environment for the problem
     TYPE(ParallelEnvType),POINTER :: pe => NULL()
@@ -56,6 +68,12 @@ MODULE MemProf
       !> @copybrief Memory_Profiler::edit_MemProf
       !> @copydetails Memory_Profiler::edit_MemProf
       PROCEDURE,PASS :: edit => edit_MemProf
+      !> @copybrief Memory_Profiler::enableEdits_MemProf
+      !> @copydetails Memory_Profiler::enableEdits_MemProf
+      PROCEDURE,PASS :: enableEdits => enableEdits_MemProf
+      !> @copybrief Memory_Profiler::disableEdits_MemProf
+      !> @copydetails Memory_Profiler::disableEdits_MemProf
+      PROCEDURE,PASS :: disableEdits => disableEdits_MemProf
   ENDTYPE Memory_Profiler
 
   !> Module name
@@ -78,6 +96,7 @@ MODULE MemProf
       TYPE(ParamType),INTENT(IN),OPTIONAL :: params
 
       INTEGER(C_LONG_LONG) :: tmpL1, tmpL2
+      CHARACTER(LEN=16) :: filename
 
       IF(ASSOCIATED(myLog)) THEN
         thisMP%pe=>pe
@@ -87,6 +106,19 @@ MODULE MemProf
         thisMP%mem_old=REAL(tmpL1,SRK)/(1024.0_SRK*1024.0_SRK)
         thisMP%mem_current=thisMP%mem_old
         thisMP%isInit=.TRUE.
+      ENDIF
+      thisMP%print_banner=.TRUE.
+      IF(PRESENT(params)) THEN
+        IF(params%has('threshold')) CALL params%get('threshold',thisMP%mem_threshold)
+        IF(params%has('verbose')) CALL params%get('verbose',thisMP%verbose)
+      ENDIF
+      thisMP%verbose=.TRUE.
+      IF(thisMP%verbose) THEN
+        WRITE(filename,'("memprof.",I0.4,".dat")') pe%world%rank
+        CALL thisMP%verbose_output%initialize(FILE=filename,STATUS='UNKNOWN')
+        CALL thisMP%verbose_output%fopen()
+        WRITE(thisMP%verbose_output%getUnitNo(),'(46x,"Used [GB] Change [MB]")')
+        FLUSH(thisMP%verbose_output%getUnitNo())
       ENDIF
     ENDSUBROUTINE init_MemProf
 !
@@ -101,6 +133,11 @@ MODULE MemProf
       thisMP%mem_old=0.0_SRK
       thisMP%pe=>NULL()
       thisMP%myLog=>NULL()
+      IF(thisMP%verbose) THEN
+        CALL thisMP%verbose_output%fclose()
+        CALL thisMP%verbose_output%clear()
+      ENDIF
+      thisMP%verbose=.FALSE.
     ENDSUBROUTINE clear_MemProf
 !
 !-------------------------------------------------------------------------------
@@ -114,29 +151,71 @@ MODULE MemProf
       CHARACTER(LEN=45)  :: tmpchar
       CHARACTER(LEN=128) :: amesg
       INTEGER(C_LONG_LONG) :: tmpL1, tmpL2
+      INTEGER(SLK) :: loc(1)
       REAL(SRK) :: mem(1), dmem(1), maxmem(1)
 
-      CALL thisMP%pe%world%barrier()
-      CALL getProcMemInfo(tmpL1,tmpL2)
-      thisMP%mem_current=REAL(tmpL1,SRK)/(1024.0_SRK*1024.0_SRK)
+      IF(thisMP%mem_edit) THEN
+        IF(thisMP%print_banner) THEN
+          IF(thisMP%pe%world%master) THEN
+            WRITE(amesg, '(47x,"Avg [GB]  Max [GB]  Max Change [MB] (rank)")')
+            CALL thisMP%myLog%message(TRIM(amesg),.FALSE.,.TRUE.)
+          ENDIF
+          thisMP%print_banner=.FALSE.
+        ENDIF
+        CALL thisMP%pe%world%barrier()
+        CALL getProcMemInfo(tmpL1,tmpL2)
+        thisMP%mem_current=REAL(tmpL1,SRK)/(1024.0_SRK*1024.0_SRK)
+        loc(1)=thisMP%pe%world%rank
 
-      mem=thisMP%mem_current
-      maxmem=mem
-      dmem=thisMP%mem_current-thisMP%mem_old
-      CALL thisMP%pe%world%allReduceMax(1,maxmem)
-      CALL thisMP%pe%world%allReduceMax(1,dmem)
-      CALL thisMP%pe%world%allReduce(1,mem)
-      mem=mem/REAL(thisMP%pe%world%nproc,SRK)
+        mem=thisMP%mem_current
+        maxmem=mem
+        dmem=(thisMP%mem_current-thisMP%mem_old)*1024.0_SRK
+        IF(thisMP%verbose) THEN
+          WRITE(tmpchar,'(a)') 'Memory Use at '//TRIM(name)//':'
+          WRITE(thisMP%verbose_output%getUnitNo(),'(a,2(f10.3))') &
+            ADJUSTL(tmpchar), mem, dmem
+          FLUSH(thisMP%verbose_output%getUnitNo())
+        ENDIF
 
-      IF(ASSOCIATED(thisMP%myLog) .AND. thisMP%pe%world%master) THEN
-        WRITE(tmpchar,'(a)') 'Memory Use at '//TRIM(name)//':'
-        WRITE(amesg,'(a,3(f10.3))') ADJUSTL(tmpchar), mem, maxmem, dmem
-        CALL thisMP%myLog%message(TRIM(amesg),.FALSE.,.TRUE.)
+        CALL thisMP%pe%world%allReduceMax(1,maxmem)
+        CALL thisMP%pe%world%ReduceMaxLoc(1,dmem,loc)
+        CALL thisMP%pe%world%allReduce(1,mem)
+        mem=mem/REAL(thisMP%pe%world%nproc,SRK)
+
+        IF(dmem(1)>=thisMP%mem_threshold .AND. ASSOCIATED(thisMP%myLog) .AND. &
+            thisMP%pe%world%master) THEN
+          WRITE(tmpchar,'(a)') 'Memory Use at '//TRIM(name)//':'
+          WRITE(amesg,'(a,3(f10.3),"  (",I4,")")') ADJUSTL(tmpchar), mem, maxmem, dmem, loc
+          CALL thisMP%myLog%message(TRIM(amesg),.FALSE.,.TRUE.)
+        ENDIF
+
+        thisMP%mem_old=thisMP%mem_current
       ENDIF
-
-      thisMP%mem_old=thisMP%mem_current
 #endif
     ENDSUBROUTINE edit_MemProf
+!
+!-------------------------------------------------------------------------------
+!> @brief Default procedure for
+!> @param thisMP the memory profiling object
+!>
+    SUBROUTINE enableEdits_MemProf(thisMP)
+      CLASS(Memory_Profiler),INTENT(INOUT) :: thisMP
+      thisMP%mem_edit=.TRUE.
+#ifndef FUTILITY_MEMPROF
+        IF(ASSOCIATED(thisMP%myLog) .AND. thisMP%pe%world%master) &
+          CALL thisMP%myLog%message("Unable to enable memory edits.  Need to " &
+            //"reconfigure with Memory Profiling on.",.FALSE.,.TRUE.)
+#endif
+    ENDSUBROUTINE enableEdits_MemProf
+!
+!-------------------------------------------------------------------------------
+!> @brief Default procedure for
+!> @param thisMP the memory profiling object
+!>
+    SUBROUTINE disableEdits_MemProf(thisMP)
+      CLASS(Memory_Profiler),INTENT(INOUT) :: thisMP
+      thisMP%mem_edit=.FALSE.
+    ENDSUBROUTINE disableEdits_MemProf
 !
 ENDMODULE MemProf
 
