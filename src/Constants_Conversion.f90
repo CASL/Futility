a50697c8b88ede92bb9f7322add8c0e7bc792363
Aaron Graham <grahamam@ornl.gov>
2018-01-10 12:01:17 -0500

    Add a few more constants to Constants_Conversion
    Added reciprocals of the constants in the previous commits, as
    well as a few other conversions that needed to be added or moved
    from other modules.

--- src/Constants_Conversion.f90
+++ src/Constants_Conversion.f90
@@ -45,15 +45,37 @@ MODULE Constants_Conversion
   !> Avogadro's Number
   REAL(SRK),PUBLIC,PARAMETER :: Na=6.02214129000E+23_SRK !16 digits
 
-  !> Conversion factor from MPa to psia (should move to constants conversion)
+  !Pressure Conversions
+  !> Conversion factor from MPa to psia
   REAL(SRK),PUBLIC,PARAMETER :: MPa2PSIA=145.0_SRK+377.0_SRK/9990.0_SRK
-  !> Conversion factor from bar to MPA
+  !> Conversion factor from bar to MPa
   REAL(SRK),PUBLIC,PARAMETER :: Bar2MPa=0.1_SRK
+  !> Conversion factor from MPa to bar
+  REAL(SRK),PUBLIC,PARAMETER :: MPa2Bar=ONE/Bar2MPa
+  !> Conversion factor from MPa to Pa
+  REAL(SRK),PUBLIC,PARAMETER :: MPa2Pa=1000000.0_SRK
+  !> Conversion factor from Pa to MPa
+  REAL(SRK),PUBLIC,PARAMETER :: Pa2MPa=ONE/MPa2Pa
 
+  !Mass conversions
   !> Conversion factor from kg to g
   REAL(SRK),PUBLIC,PARAMETER :: kg2g=1000.0_SRK
+  !> Conversion factor from g to kg
+  REAL(SRK),PUBLIC,PARAMETER :: g2kg=ONE/kg2g
+  !> Conversion factor from g to mg
+  REAL(SRK),PUBLIC,PARAMETER :: g2mg=1000.0_SRK
+  !> Conversion factor from mg to g
+  REAL(SRK),PUBLIC,PARAMETER :: mg2g=ONE/g2mg
+
+  !Length conversions
   !> Conversion factor from m to cm
   REAL(SRK),PUBLIC,PARAMETER :: m2cm=100.0_SRK
+  !> Conversion factor from cm to m
+  REAL(SRK),PUBLIC,PARAMETER :: cm2m=ONE/m2cm
+
+  !Temperature conversions
+  !> Conversion factor from Celsius to Kelvin
+  REAL(SRK),PUBLIC,PARAMETER :: C2K=273.15_SRK
 
   !> May be used for comparing real numbers relating to geometry
   !> Tolerance is 0.0001 cm
