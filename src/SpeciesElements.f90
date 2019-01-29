905f1781c1b6cacfa9d8524f79fe86236c2f6a06
Zack Taylor <rtaylo45@vols.utk.edu>
2019-01-29 15:00:37 -0500

    Added Species to elements type. This houses a routine that converts a chemical formula into a 118 size array with the element atomic number corresponding to the array index and the value in the index corresponds to the stoichiometric coefficient

--- src/SpeciesElements.f90
+++ src/SpeciesElements.f90
@@ -30,7 +30,7 @@ MODULE SpeciesElements
   USE Futility_DBC
   USE IntrType
   USE ExceptionHandler
-  USE IO_Strings
+  USE Strings
   USE ElementsIsotopes
   IMPLICIT NONE
   PRIVATE
@@ -38,26 +38,17 @@ MODULE SpeciesElements
   ! Member
   PUBLIC :: SpeciesElementsType
 
-  ! List of capital letters
-  CHARACTER(LEN=1) :: capitalList=(/'A','B','C','D','E','F','G','H','I','J',&
-     'K','L','M','N','O','P','Q','R','S','T','U','P','W','X','Y','Z'/)
-  ! List of lower case letters
-  CHARACTER(LEN=1) :: lowerList=(/'a','b','c','d','e','f','g','h','i','j',&
-     'k','l','m','n','o','p','q','r','s','t','u','p','w','x','y','z'/)
-
   !> Type that converts a given species chemical formula and molar amount
   !! to an array of molar amounts ordered by atomic number 
   TYPE :: SpeciesElementsType 
-    !> Initialization status (not really needed)
-    LOGICAL(SBK) :: isInit=.FALSE.
 
 
-!List of type bound procedures
+  !List of type bound procedures
   CONTAINS
-    PROCEDURE,PASS :: init
-    PROCEDURE,PASS :: clear
-    PROCEDURE,PASS,PRIVATE :: splitString
     PROCEDURE,PASS :: getElementArray
+    PROCEDURE,PASS,PRIVATE :: isChar
+    PROCEDURE,PASS,PRIVATE :: isCharCap
+    PROCEDURE,PASS,PRIVATE :: isCharLow
   ENDTYPE SpeciesElementsType
 
 !
@@ -65,17 +56,120 @@ MODULE SpeciesElements
   CONTAINS
 !
 !-------------------------------------------------------------------------------
-!> @brief Constructor for the species to elements converter
-!> @param this the variable to initialize
+!> @brief Returns a 0:118 size array with the atomic number index containing
+!> the stoichiometric coefficient in the input formula
+!> @param formula chemical formula
 !>
-!> The constructor for the species to elements converter
-!>
-    SUBROUTINE init(this)
+   FUNCTION getElementArray(this, formula) RESULT(eleArray)
       CLASS(SpeciesElementsType),INTENT(INOUT) :: this
-      
-      this%isInit=.TRUE.
-    ENDSUBROUTINE init
+      type(StringType), INTENT(IN) :: formula
 
+      REAL(SDK), dimension(0:118) :: eleArray ! Return Value
+      INTEGER(SIK) :: i, Z
+      REAL(SDK) :: coeff
+      CHARACTER(LEN=1) :: letter, nextLetter
+      CHARACTER(LEN=2) :: element
+      TYPE(ElementsIsotopesType) :: myEI
 
+      eleArray = 0.0
+
+      DO i=1, LEN(formula)
+         coeff = 1.0
+         ! Gets the value in the formula
+         letter = CHAR(formula,i,i)
+         ! Test to see if its a letter         
+         IF (this%isChar(letter)) THEN
+            ! Test to see if its capital
+            IF (this%isCharCap(letter)) THEN
+               element = letter
+               IF (i /= LEN(formula)) THEN
+                  ! Gets the next letter to see if its lower or upper.
+                  nextLetter = CHAR(formula,i+1,i+1)
+               ELSE
+                  nextLetter = 'A'
+               END IF
+               ! If its lower case then it needs to be added to the 
+               ! current letter
+               IF (this%isCharLow(nextLetter)) THEN
+                  element = letter // nextLetter
+               END IF
+            END IF
+         ! If the character is lower case then do nothing
+         ELSE IF (this%isCharLow(letter)) THEN
+            CONTINUE 
+         ! If not a letter then its a stoichiometrix coefficient
+         ELSE
+            coeff = letter
+         END IF
+         ! Gets the atomic number
+         Z = myEI%getAtomicNumber(element)
+         ! Sets the coeff in the array
+         eleArray(Z) = coeff 
+      END DO
+   ENDFUNCTION  getElementArray
 !
-ENDMODULE SpeciesElementsType
+!-------------------------------------------------------------------------------
+!> @brief Determins whether a character is in the alphabet using ASCII format
+!> @param letter character to check
+!>
+   FUNCTION isChar(this, letter) RESULT(isValid)
+      CLASS(SpeciesElementsType),INTENT(INOUT) :: this
+      CHARACTER(LEN=*), INTENT(IN) :: letter
+
+      LOGICAL(SBK) :: isValid
+      INTEGER(SIK) :: val
+
+      val = IACHAR(letter)
+
+      isValid = .FALSE.
+      ! Checks against the upper case letters
+      IF (65<= val .AND. val<=90) THEN
+         isValid = .TRUE.
+      ! Checks against the lower case letters
+      ELSE IF  (97<= val .AND. val<=122) THEN
+         isValid = .TRUE.
+      end if 
+
+   ENDFUNCTION isChar 
+!
+!-------------------------------------------------------------------------------
+!> @brief Determins whether a character is capitilized using ASCII format
+!> @param letter character to check
+!>
+   FUNCTION isCharCap(this, letter) RESULT(isValid)
+      CLASS(SpeciesElementsType),INTENT(INOUT) :: this
+      CHARACTER(LEN=*), INTENT(IN) :: letter
+
+      LOGICAL(SBK) :: isValid
+      INTEGER(SIK) :: val
+
+      val = IACHAR(letter)
+
+      isValid = .FALSE.
+      IF (65<= val .AND. val<=90) THEN
+         isValid = .TRUE.
+      END IF 
+
+   ENDFUNCTION isCharCap
+!
+!-------------------------------------------------------------------------------
+!> @brief Determins whether a character is lower case using ASCII format
+!> @param letter character to check
+!>
+   FUNCTION isCharLow(this, letter) RESULT(isValid)
+      CLASS(SpeciesElementsType),INTENT(INOUT) :: this
+      CHARACTER(LEN=*), INTENT(IN) :: letter
+
+      LOGICAL(SBK) :: isValid
+      INTEGER(SIK) :: val
+
+      val = IACHAR(letter)
+
+      isValid = .FALSE.
+      IF (97<= val .AND. val<=122) THEN
+         isValid = .TRUE.
+      END IF 
+
+   ENDFUNCTION isCharLow
+!
+ENDMODULE SpeciesElements
