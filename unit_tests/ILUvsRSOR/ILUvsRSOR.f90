355fb0c2bf09248423fcdf905e2e1ae482b80778
Nicholas Herring <nfherrin@umich.edu>
2019-02-01 11:12:02 -0500

    changed ilutest

--- unit_tests/ILUvsRSOR/ILUvsRSOR.f90
+++ unit_tests/ILUvsRSOR/ILUvsRSOR.f90
@@ -68,10 +68,18 @@ PROGRAM ILUvsRSOR
   CALL mpiTestEnv%init(PE_COMM_SELF)
 #endif
 
-  CREATE_TEST('Test RSOR Preconditioner')
+  CREATE_TEST('RSOR Speed')
 
   CALL setupILUvsRSOR()
-  REGISTER_SUBTEST('Test RSOR Preconditioner Type',testILUvsRSOR)
+  REGISTER_SUBTEST('Test NoPrecon Speed',NoPreconSpeed)
+  CALL clearTest()
+
+  CALL setupILUvsRSOR()
+  REGISTER_SUBTEST('Test RSOR Speed',RSORSpeed)
+  CALL clearTest()
+  
+  CALL setupILUvsRSOR()
+  REGISTER_SUBTEST('Test ILU Speed',ILUSpeed)
   CALL clearTest()
   
   FINALIZE_TEST()
@@ -104,8 +112,6 @@ PROGRAM ILUvsRSOR
         ASSERT(ios .EQ. 0, 'got directory correctly')
         
         WRITE(dirname,'(2A)')ADJUSTL(TRIM(tempcharacter)),'/unit_tests/ILUvsRSOR/matrices/mg_matrix.txt'
-        
-        WRITE(*,*)dirname
 
         OPEN(UNIT=11,FILE=dirname,STATUS='OLD',ACTION='READ',IOSTAT=ios,IOMSG=tempcharacter)
         IF(ios .NE. 0)THEN
@@ -140,130 +146,89 @@ PROGRAM ILUvsRSOR
     ENDSUBROUTINE setupILUvsRSOR
 !
 !-------------------------------------------------------------------------------
-    SUBROUTINE testILUvsRSOR()
+    SUBROUTINE NoPreconSpeed()
         CLASS(SOR_PreCondType),ALLOCATABLE :: testSOR
         CLASS(LU_PreCondType),ALLOCATABLE :: testLU
         CLASS(LinearSolverType_Base),ALLOCATABLE :: thisGMRES
         REAL(SRK),POINTER :: thisX(:)
-        INTEGER(SIK)::i,j,k
-        REAL(SRK)::tmpreal
+        INTEGER(SIK)::i,j,k,time1,time2,clock_rate
+        REAL(SRK)::tmpreal,timetaken
         
         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         !No preconditioner
         
-        !ALLOCATE(LinearSolverType_Iterative :: thisGMRES)
-        !
-        !! initialize linear system
-        !CALL pList%clear()
-        !CALL pList%add('LinearSolverType->TPLType',NATIVE)
-        !CALL pList%add('LinearSolverType->solverMethod',GMRES)
-        !CALL pList%add('LinearSolverType->MPI_Comm_ID',PE_COMM_SELF)
-        !CALL pList%add('LinearSolverType->numberOMP',1_SNK)
-        !CALL pList%add('LinearSolverType->timerName','testTimer')
-        !CALL pList%add('LinearSolverType->matType',DENSESQUARE)
-        !CALL pList%add('LinearSolverType->A->MatrixType->n',1512_SNK)
-        !CALL pList%add('LinearSolverType->A->MatrixType->isSym',.FALSE.)
-        !CALL pList%add('LinearSolverType->x->VectorType->n',1512_SNK)
-        !CALL pList%add('LinearSolverType->b->VectorType->n',1512_SNK)
-        !CALL pList%validate(pList,optListLS)
-        !CALL thisGMRES%init(pList)
-        !
-        !! build X0 and set it to 1.0s
-        !ALLOCATE(thisX(1512))
-        !thisX=1.0_SRK
-        !
-        !SELECTTYPE(thisGMRES); TYPE IS(LinearSolverType_Iterative)
-        !    CALL thisGMRES%setX0(thisX)
-        !ENDSELECT
-        !
-        !SELECTTYPE(A => thisGMRES%A); TYPE IS(DenseSquareMatrixType)
-        !    DO i=1,1512
-        !        DO j=1,1512
-        !            CALL testDenseMatrix%get(i,j,tmpreal)
-        !            CALL A%set(i,j,tmpreal)
-        !        END DO
-        !    END DO
-        !ENDSELECT
-        !
-        !SELECTTYPE(b => thisGMRES%b); TYPE IS(RealVectorType)
-        !    DO i=1,1512
-        !            CALL rhsVector%get(i,tmpreal)
-        !            CALL b%set(i,tmpreal)
-        !    END DO
-        !ENDSELECT
-        !
-        !!set iterations and convergence information and build/set M
-        !SELECTTYPE(thisGMRES); TYPE IS(LinearSolverType_Iterative)
-        !    CALL thisGMRES%setConv(2_SIK,1.0E-9_SRK,1.0E-9_SRK,1000_SIK,1512_SIK)
-        !ENDSELECT
-        !
-        !CALL thisGMRES%solve()
-        !SELECTTYPE(thisGMRES); TYPE IS(LinearSolverType_Iterative)
-        !    WRITE(*,'(A,I0)')'No Precondition Iterations ',thisGMRES%iters
-        !    WRITE(*,'(A,ES28.18)')'No Precondition Residual ',thisGMRES%residual
-        !ENDSELECT
-        !DEALLOCATE(thisGMRES)
-        !
-        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        !!ILU preconditioner
-        !ALLOCATE(LinearSolverType_Iterative :: thisGMRES)
-        !
-        !! initialize linear system
-        !CALL pList%clear()
-        !CALL pList%add('LinearSolverType->TPLType',NATIVE)
-        !CALL pList%add('LinearSolverType->solverMethod',GMRES)
-        !CALL pList%add('LinearSolverType->MPI_Comm_ID',PE_COMM_SELF)
-        !CALL pList%add('LinearSolverType->numberOMP',1_SNK)
-        !CALL pList%add('LinearSolverType->timerName','testTimer')
-        !CALL pList%add('LinearSolverType->matType',DENSESQUARE)
-        !CALL pList%add('LinearSolverType->A->MatrixType->n',1512_SNK)
-        !CALL pList%add('LinearSolverType->A->MatrixType->isSym',.FALSE.)
-        !CALL pList%add('LinearSolverType->x->VectorType->n',1512_SNK)
-        !CALL pList%add('LinearSolverType->b->VectorType->n',1512_SNK)
-        !CALL pList%validate(pList,optListLS)
-        !CALL thisGMRES%init(pList)
-        !
-        !! build X0 and set it to 1.0s
-        !ALLOCATE(thisX(1512))
-        !thisX=1.0_SRK
-        !
-        !SELECTTYPE(thisGMRES); TYPE IS(LinearSolverType_Iterative)
-        !    CALL thisGMRES%setX0(thisX)
-        !ENDSELECT
-        !
-        !SELECTTYPE(A => thisGMRES%A); TYPE IS(DenseSquareMatrixType)
-        !    DO i=1,1512
-        !        DO j=1,1512
-        !            CALL testDenseMatrix%get(i,j,tmpreal)
-        !            CALL A%set(i,j,tmpreal)
-        !        END DO
-        !    END DO
-        !ENDSELECT
-        !
-        !SELECTTYPE(b => thisGMRES%b); TYPE IS(RealVectorType)
-        !    DO i=1,1512
-        !            CALL rhsVector%get(i,tmpreal)
-        !            CALL b%set(i,tmpreal)
-        !    END DO
-        !ENDSELECT
-        !
-        !SELECTTYPE(thisGMRES); TYPE IS(LinearSolverType_Iterative)
-        !    ALLOCATE(ILU_PreCondType :: thisGMRES%PreCondType)
-        !    CALL thisGMRES%setupPC()
-        !    thisGMRES%pciters=1
-        !ENDSELECT
-        !
-        !!set iterations and convergence information and build/set M
-        !SELECTTYPE(thisGMRES); TYPE IS(LinearSolverType_Iterative)
-        !    CALL thisGMRES%setConv(2_SIK,1.0E-9_SRK,1.0E-9_SRK,1000_SIK,1512_SIK)
-        !ENDSELECT
-        !
-        !CALL thisGMRES%solve()
-        !SELECTTYPE(thisGMRES); TYPE IS(LinearSolverType_Iterative)
-        !    WRITE(*,'(A,I0)')'ILU Iterations ',thisGMRES%iters
-        !    WRITE(*,'(A,ES28.18)')'ILU Residual ',thisGMRES%residual
-        !ENDSELECT
-        !DEALLOCATE(thisGMRES)
+        ALLOCATE(LinearSolverType_Iterative :: thisGMRES)
+        
+        ! initialize linear system
+        CALL pList%clear()
+        CALL pList%add('LinearSolverType->TPLType',NATIVE)
+        CALL pList%add('LinearSolverType->solverMethod',GMRES)
+        CALL pList%add('LinearSolverType->MPI_Comm_ID',PE_COMM_SELF)
+        CALL pList%add('LinearSolverType->numberOMP',1_SNK)
+        CALL pList%add('LinearSolverType->timerName','testTimer')
+        CALL pList%add('LinearSolverType->matType',DENSESQUARE)
+        CALL pList%add('LinearSolverType->A->MatrixType->n',1512_SNK)
+        CALL pList%add('LinearSolverType->A->MatrixType->isSym',.FALSE.)
+        CALL pList%add('LinearSolverType->x->VectorType->n',1512_SNK)
+        CALL pList%add('LinearSolverType->b->VectorType->n',1512_SNK)
+        CALL pList%validate(pList,optListLS)
+        CALL thisGMRES%init(pList)
+        
+        ! build X0 and set it to 1.0s
+        ALLOCATE(thisX(1512))
+        thisX=1.0_SRK
+        
+        SELECTTYPE(thisGMRES); TYPE IS(LinearSolverType_Iterative)
+            CALL thisGMRES%setX0(thisX)
+        ENDSELECT
+        
+        SELECTTYPE(A => thisGMRES%A); TYPE IS(DenseSquareMatrixType)
+            DO i=1,1512
+                DO j=1,1512
+                    CALL testDenseMatrix%get(i,j,tmpreal)
+                    CALL A%set(i,j,tmpreal)
+                END DO
+            END DO
+        ENDSELECT
+        
+        SELECTTYPE(b => thisGMRES%b); TYPE IS(RealVectorType)
+            DO i=1,1512
+                    CALL rhsVector%get(i,tmpreal)
+                    CALL b%set(i,tmpreal)
+            END DO
+        ENDSELECT
+        
+        !set iterations and convergence information and build/set M
+        SELECTTYPE(thisGMRES); TYPE IS(LinearSolverType_Iterative)
+            CALL thisGMRES%setConv(2_SIK,1.0E-9_SRK,1.0E-9_SRK,1000_SIK,1512_SIK)
+        ENDSELECT
+        
+        !call clock right before parallel part in the SOR iterations
+        CALL SYSTEM_CLOCK(time1)
+        CALL thisGMRES%solve()
+        !call clock right after parallel part in the SOR iterations, this will give us how long the SOR iterations took to converge
+        CALL SYSTEM_CLOCK(time2,clock_rate)
+        !calculate time taken in seconds
+        timetaken=(time2*1.0_SRK-time1*1.0_SRK)/(clock_rate*1.0_SRK)
+        SELECTTYPE(thisGMRES); TYPE IS(LinearSolverType_Iterative)
+            WRITE(*,'(A,I0)')'No Precondition Iterations ',thisGMRES%iters
+            WRITE(*,'(A,ES28.18)')'No Precondition Time Taken ',timetaken
+            WRITE(*,'(A,ES28.18)')'No Precondition Time Per Iteration ',timetaken/thisGMRES%iters
+            WRITE(*,'(A,ES28.18)')'No Precondition Residual ',thisGMRES%residual
+        ENDSELECT
+        
+        DEALLOCATE(thisGMRES)
+
+    ENDSUBROUTINE NoPreconSpeed
+!
+!-------------------------------------------------------------------------------
+    SUBROUTINE RSORSpeed()
+        CLASS(SOR_PreCondType),ALLOCATABLE :: testSOR
+        CLASS(LU_PreCondType),ALLOCATABLE :: testLU
+        CLASS(LinearSolverType_Base),ALLOCATABLE :: thisGMRES
+        REAL(SRK),POINTER :: thisX(:)
+        INTEGER(SIK)::i,j,k,time1,time2,clock_rate
+        REAL(SRK)::tmpreal,timetaken
         
         !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         !RSOR preconditioner
@@ -310,11 +275,15 @@ PROGRAM ILUvsRSOR
         
         SELECTTYPE(thisGMRES); TYPE IS(LinearSolverType_Iterative)
             ALLOCATE(RSOR_PreCondType :: thisGMRES%PreCondType)
+            !call clock right before parallel part in the SOR iterations
+            CALL SYSTEM_CLOCK(time1)
             CALL thisGMRES%setupPC(PListRSOR)
+            !call clock right after parallel part in the SOR iterations, this will give us how long the SOR iterations took to converge
+            CALL SYSTEM_CLOCK(time2,clock_rate)
+            !calculate time taken in seconds
+            timetaken=(time2*1.0_SRK-time1*1.0_SRK)/(clock_rate*1.0_SRK)
+            WRITE(*,'(A,ES28.18)')'RSOR Setup Time ',timetaken
             thisGMRES%pciters=1
-            SELECTTYPE(precon => thisGMRES%PreCondType); TYPE IS(RSOR_PreCondType)
-                WRITE(*,'(A,F8.4)')'Omega ',precon%omega
-            ENDSELECT
         ENDSELECT
 
         !set iterations and convergence information and build/set M
@@ -322,60 +291,107 @@ PROGRAM ILUvsRSOR
             CALL thisGMRES%setConv(2_SIK,1.0E-9_SRK,1.0E-9_SRK,1000_SIK,1512_SIK)
         ENDSELECT
         
+        !call clock right before parallel part in the SOR iterations
+        CALL SYSTEM_CLOCK(time1)
         CALL thisGMRES%solve()
-        WRITE(*,*)'We get here',3
+        !call clock right after parallel part in the SOR iterations, this will give us how long the SOR iterations took to converge
+        CALL SYSTEM_CLOCK(time2,clock_rate)
+        !calculate time taken in seconds
+        timetaken=(time2*1.0_SRK-time1*1.0_SRK)/(clock_rate*1.0_SRK)
         SELECTTYPE(thisGMRES); TYPE IS(LinearSolverType_Iterative)
             WRITE(*,'(A,I0)')'RSOR Iterations ',thisGMRES%iters
+            WRITE(*,'(A,ES28.18)')'RSOR Time Taken ',timetaken
+            WRITE(*,'(A,ES28.18)')'RSOR Time Per Iteration ',timetaken/thisGMRES%iters
             WRITE(*,'(A,ES28.18)')'RSOR Residual ',thisGMRES%residual
         ENDSELECT
         
-        !troubleshooting
-        !SELECTTYPE(thisGMRES); TYPE IS(LinearSolverType_Iterative)
-        !    !WRITE(*,*)'AMatrix'
-        !    !DO i=1,1512
-        !    !    DO j=1,1512
-        !    !        CALL thisGMRES%A%get(i,j,tmpreal)
-        !    !        WRITE(*,'(ES28.18)',ADVANCE='NO')tmpreal
-        !    !    END DO
-        !    !    WRITE(*,*)
-        !    !END DO
-        !    SELECTTYPE(precon => thisGMRES%PreCondType); TYPE IS(RSOR_PreCondType)
-        !        !WRITE(*,*)'LpUMatrix'
-        !        !DO i=1,1512
-        !        !    DO j=1,1512
-        !        !        CALL precon%LpU%get(i,j,tmpreal)
-        !        !        WRITE(*,'(ES28.18)',ADVANCE='NO')tmpreal
-        !        !    END DO
-        !        !    WRITE(*,*)
-        !        !END DO
-        !        WRITE(*,*)'LUofBlocks'
-        !        DO k=1,precon%numblocks
-        !            !WRITE(*,'(A,I0)')'Block',k
-        !            DO i=1,precon%blocksize
-        !                DO j=1,precon%blocksize
-        !                    CALL precon%LU(k)%get(i,j,tmpreal)
-        !                    WRITE(*,'(ES28.18)',ADVANCE='NO')tmpreal
-        !                END DO
-        !            WRITE(*,*)
-        !            END DO
-        !        END DO
-        !    ENDSELECT
-        !    !WRITE(*,*)'RHSVector'
-        !    !DO i=1,1512
-        !    !    CALL thisGMRES%b%get(i,tmpreal)
-        !    !    WRITE(*,'(ES28.18)')tmpreal
-        !    !END DO
-        !    WRITE(*,*)'AppliedToRHS'
-        !    CALL thisGMRES%PreCondType%apply(thisGMRES%b)
-        !    DO i=1,1512
-        !        CALL thisGMRES%b%get(i,tmpreal)
-        !        WRITE(*,'(ES28.18)')tmpreal
-        !    END DO
-        !ENDSELECT
-        
         DEALLOCATE(thisGMRES)
 
-    ENDSUBROUTINE testILUvsRSOR
+    ENDSUBROUTINE RSORSpeed
+!
+!-------------------------------------------------------------------------------
+    SUBROUTINE ILUSpeed()
+        CLASS(SOR_PreCondType),ALLOCATABLE :: testSOR
+        CLASS(LU_PreCondType),ALLOCATABLE :: testLU
+        CLASS(LinearSolverType_Base),ALLOCATABLE :: thisGMRES
+        REAL(SRK),POINTER :: thisX(:)
+        INTEGER(SIK)::i,j,k,time1,time2,clock_rate
+        REAL(SRK)::tmpreal,timetaken
+        
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        !ILU preconditioner
+        ALLOCATE(LinearSolverType_Iterative :: thisGMRES)
+        
+        ! initialize linear system
+        CALL pList%clear()
+        CALL pList%add('LinearSolverType->TPLType',NATIVE)
+        CALL pList%add('LinearSolverType->solverMethod',GMRES)
+        CALL pList%add('LinearSolverType->MPI_Comm_ID',PE_COMM_SELF)
+        CALL pList%add('LinearSolverType->numberOMP',1_SNK)
+        CALL pList%add('LinearSolverType->timerName','testTimer')
+        CALL pList%add('LinearSolverType->matType',DENSESQUARE)
+        CALL pList%add('LinearSolverType->A->MatrixType->n',1512_SNK)
+        CALL pList%add('LinearSolverType->A->MatrixType->isSym',.FALSE.)
+        CALL pList%add('LinearSolverType->x->VectorType->n',1512_SNK)
+        CALL pList%add('LinearSolverType->b->VectorType->n',1512_SNK)
+        CALL pList%validate(pList,optListLS)
+        CALL thisGMRES%init(pList)
+        
+        ! build X0 and set it to 1.0s
+        ALLOCATE(thisX(1512))
+        thisX=1.0_SRK
+        
+        SELECTTYPE(thisGMRES); TYPE IS(LinearSolverType_Iterative)
+            CALL thisGMRES%setX0(thisX)
+        ENDSELECT
+        
+        SELECTTYPE(A => thisGMRES%A); TYPE IS(DenseSquareMatrixType)
+            DO i=1,1512
+                DO j=1,1512
+                    CALL testDenseMatrix%get(i,j,tmpreal)
+                    CALL A%set(i,j,tmpreal)
+                END DO
+            END DO
+        ENDSELECT
+        
+        SELECTTYPE(b => thisGMRES%b); TYPE IS(RealVectorType)
+            DO i=1,1512
+                    CALL rhsVector%get(i,tmpreal)
+                    CALL b%set(i,tmpreal)
+            END DO
+        ENDSELECT
+        
+        SELECTTYPE(thisGMRES); TYPE IS(LinearSolverType_Iterative)
+            ALLOCATE(ILU_PreCondType :: thisGMRES%PreCondType)
+            CALL SYSTEM_CLOCK(time1)
+            CALL thisGMRES%setupPC()
+            CALL SYSTEM_CLOCK(time2,clock_rate)
+            timetaken=(time2*1.0_SRK-time1*1.0_SRK)/(clock_rate*1.0_SRK)
+            WRITE(*,'(A,ES28.18)')'ILU Setup Time ',timetaken
+            thisGMRES%pciters=1
+        ENDSELECT
+        
+        !set iterations and convergence information and build/set M
+        SELECTTYPE(thisGMRES); TYPE IS(LinearSolverType_Iterative)
+            CALL thisGMRES%setConv(2_SIK,1.0E-9_SRK,1.0E-9_SRK,1000_SIK,1512_SIK)
+        ENDSELECT
+        
+        !call clock right before parallel part in the SOR iterations
+        CALL SYSTEM_CLOCK(time1)
+        CALL thisGMRES%solve()
+        !call clock right after parallel part in the SOR iterations, this will give us how long the SOR iterations took to converge
+        CALL SYSTEM_CLOCK(time2,clock_rate)
+        !calculate time taken in seconds
+        timetaken=(time2*1.0_SRK-time1*1.0_SRK)/(clock_rate*1.0_SRK)
+        SELECTTYPE(thisGMRES); TYPE IS(LinearSolverType_Iterative)
+            WRITE(*,'(A,I0)')'ILU Iterations ',thisGMRES%iters
+            WRITE(*,'(A,ES28.18)')'ILU Time Taken ',timetaken
+            WRITE(*,'(A,ES28.18)')'ILU Time Per Iteration ',timetaken/thisGMRES%iters
+            WRITE(*,'(A,ES28.18)')'ILU Residual ',thisGMRES%residual
+        ENDSELECT
+        DEALLOCATE(thisGMRES)
+
+    ENDSUBROUTINE ILUSpeed
 !
 !-------------------------------------------------------------------------------
     SUBROUTINE clearTest()
