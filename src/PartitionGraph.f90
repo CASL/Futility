74a27cffcfbcab3345abffb036dbacb6d83db8db
Aaron Graham <grahamam@ornl.gov>
2019-11-14 23:03:49 -0500

    Revert "Making the maxnsr output argument optional."
    This reverts commit 7e5dc27878a7464cb03a5dda16117d82836b838d.

--- src/PartitionGraph.f90
+++ src/PartitionGraph.f90
@@ -1726,34 +1726,31 @@ MODULE PartitionGraph
 !> @param ecut the total weight of edges cut
 !> @param comm the total weight of communication between groups
 !>
-    SUBROUTINE calcDecompMetrics_PartitionGraph(thisGraph,mmr,srms,ecut,comm,maxnsr)
+    SUBROUTINE calcDecompMetrics_PartitionGraph(thisGraph,maxnsr,mmr,srms,ecut,comm)
       CHARACTER(LEN=*),PARAMETER :: myName='calcDecompMetrics'
       CLASS(PartitionGraphType),INTENT(IN) :: thisGraph
-      REAL(SRK),INTENT(OUT) :: mmr,srms,ecut,comm
-      REAL(SRK),INTENT(OUT),OPTIONAL :: maxnsr
+      REAL(SRK),INTENT(OUT) :: maxnsr,mmr,srms,ecut,comm
       INTEGER(SIK) :: ig,igstt,igstp,in,ineigh,iv,ivert,neighGrp
       REAL(SRK) :: wtSum,wtGrp,lgroup,sgroup,optSize,wtDif,gwt
       INTEGER(SIK),ALLOCATABLE :: grpMap(:),uniqueGrps(:)
 
-      IF(PRESENT(maxnsr)) maxnsr=0.0_SRK
+      maxnsr=0.0_SRK
       mmr=0.0_SRK
       srms=0.0_SRK
       ecut=0.0_SRK
       comm=0.0_SRK
       IF(ALLOCATED(thisGraph%groupIdx)) THEN
         !Compute the max number of source regions
-        IF(PRESENT(maxnsr)) THEN
-          DO ig=1,thisGraph%nGroups
-            igstt=thisGraph%groupIdx(ig)
-            igstp=thisGraph%groupIdx(ig+1)-1
-            gwt=0.0_SRK
-            DO iv=igstt,igstp
-              gwt=gwt+thisGraph%wts(thisGraph%groupList(iv))
-            ENDDO
-            maxnsr=MAX(maxnsr,REAL(gwt,SRK))
+        DO ig=1,thisGraph%nGroups
+          igstt=thisGraph%groupIdx(ig)
+          igstp=thisGraph%groupIdx(ig+1)-1
+          gwt=0.0_SRK
+          DO iv=igstt,igstp
+            gwt=gwt+thisGraph%wts(thisGraph%groupList(iv))
           ENDDO
-          maxnsr=maxnsr/SUM(thisGraph%wts)
-        ENDIF
+          maxnsr=MAX(maxnsr,REAL(gwt,SRK))
+        ENDDO
+        maxnsr=maxnsr/SUM(thisGraph%wts)
 
         !Compute the min/max ratio and rms difference from optimal
         lgroup=0.0_SRK
@@ -1824,9 +1821,7 @@ MODULE PartitionGraph
           ' - graph is not partitioned!')
       ENDIF
 
-      IF(PRESENT(maxnsr)) THEN
-        ENSURE((maxnsr > 0.0_SRK) .AND. (maxnsr <= 1.0_SRK))
-      ENDIF
+      ENSURE((maxnsr > 0.0_SRK) .AND. (maxnsr <= 1.0_SRK))
       ENSURE(mmr > 0.0_SRK)
       ENSURE((srms > 0.0_SRK) .AND. (srms <= 100.0_SRK))
       ENSURE(ecut >= 0.0_SRK)
