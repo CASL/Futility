36b16d511a1eb67d7b73c5ece9000ab4579e7227
Sooyoung Choi <sooychoi@umich.edu>
2020-01-08 16:11:26 -0700

    MacroMOC - Constants

---
+++ src/Constants_Conversion.hpp
@@ -0,0 +1,17 @@
+#pragma once
+namespace Futility {
+static constexpr double const ZERO=0.000000000000000;
+static constexpr double const ONE=1.000000000000000;
+static constexpr double const TWO=2.000000000000000;
+static constexpr double const HALF=0.500000000000000;
+static constexpr double const QTRPI=0.7853981633974483;
+static constexpr double const HALFPI=TWO*QTRPI;
+static constexpr double const PI=TWO*HALFPI;
+static constexpr double const SIXTHPI=PI/6.0;
+static constexpr double const THIRDPI=TWO*SIXTHPI;
+static constexpr double const TWOPI=TWO*PI;
+static constexpr double const FOURPI=TWO*TWOPI;
+static constexpr double const RPI=ONE/PI;
+static constexpr double const RTWOPI=ONE/TWOPI;
+static constexpr double const RFOURPI=ONE/FOURPI;
+} // namespace Futility
