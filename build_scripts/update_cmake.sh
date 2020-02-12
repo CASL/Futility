ca1e6da9e7cd042a37959a997223644671c8fdae
Benjamin Collins <collinsbs@ornl.gov>
2020-02-12 08:57:16 -0500

    testing

---
+++ build_scripts/update_cmake.sh
@@ -0,0 +1,13 @@
+apt-get purge cmake
+
+version=3.10
+build=3
+mkdir ~/temp
+cd ~/temp
+wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
+tar -xzvf cmake-$version.$build.tar.gz
+cd cmake-$version.$build/
+
+./bootstrap
+make -j4
+make install
