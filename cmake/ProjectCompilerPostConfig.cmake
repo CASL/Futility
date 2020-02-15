28361819d7279f3c8a55bdb9710bd3f63f67243d
bscollin <collinsbs@ornl.gov>
2020-02-15 17:31:42 -0500

    Fixes case where Kokkos isn't setting the host space when OpenMP is on (PHI-5294) (#227)

--- cmake/ProjectCompilerPostConfig.cmake
+++ cmake/ProjectCompilerPostConfig.cmake
@@ -1,3 +1,11 @@
+# Set Kokkos host spaces
+IF(${PROJECT_NAME}_ENABLE_Kokkos)
+  SET(KOKKOS_ENABLE_SERIAL TRUE)
+  IF(${PROJECT_NAME}_ENABLE_OpenMP)
+    SET(KOKKOS_ENABLE_OPENMP TRUE)
+  ENDIF()
+ENDIF()
+
 if(Trilinos_SOURCE_DIR)
   include(${Trilinos_SOURCE_DIR}/cmake/ProjectCompilerPostConfig.cmake)
 endif()
