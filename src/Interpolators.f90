504e48ebbb959a2d913f049f49781276056a565d
Jesse Jones <jonesjp@ornl.gov>
2020-03-03 15:48:50 -0500

    Cleanup of doxygen documentation and variable names.

--- src/Interpolators.f90
+++ src/Interpolators.f90
@@ -8,7 +8,8 @@
 !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
 !> @brief A Fortran 2003 module implementing interpolation routines for input
 !>        arrays returning either interpolated values or a flat "extrapolation"
-!>        when interpolation is out of range.
+!>        when  requested interpolation is out of range. Right now only 1D, 2D,
+!>        and 3D routines are available.
 !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
 MODULE Interpolators
 
@@ -36,44 +37,49 @@ CONTAINS
 !
 !-------------------------------------------------------------------------------
 !> @brief This routine takes a 1D data "table" and linearly interpolates to find
-!>        a desired point.input arrays and interpolates as needed. It is important
-!>        to note that if an out of range extrapolation is required a flat extension
-!>        of the table data is performed.
-!>
-FUNCTION Interp_1D(labels,table,pp) RESULT(p)
-  REAL(SRK),INTENT(IN) :: labels(:),table(:),pp
-  REAL(SRK) :: p,f(2)
+!>        a desired point.
+!> @param labels axis labels at which data points in table are defined
+!> @param table contains data to be interpolated between defined at points
+!>        given in labels
+!> @param axis_p point at which interpolation needs to be done to return a value
+!> @param data_p interpolated data found through linear interpolation to the point axis_p
+FUNCTION Interp_1D(labels,table,axis_p) RESULT(data_p)
+  REAL(SRK),INTENT(IN) :: labels(:),table(:),axis_p
+  REAL(SRK) :: data_p,f(2)
   INTEGER(SIK) :: i,N_i,i_p
 
-  CALL Get_points_and_weights(labels,pp,f,i_p,N_i)
+  CALL Get_points_and_weights(labels,axis_p,f,i_p,N_i)
 
   !Get interpolated value
-  p=0.0_SRK
+  data_p=0.0_SRK
   DO i=MAX(i_p-1,1),MIN(i_p,N_i)
-    p=p+f(i-i_p+2)*table(i)
+    data_p=data_p+f(i-i_p+2)*table(i)
   ENDDO
 
 ENDFUNCTION Interp_1D
 !
 !-------------------------------------------------------------------------------
 !> @brief This routine takes a 2D data table and linearly interpolates to find
-!>        a desired point.input arrays and interpolates as needed. It is important
-!>        to note that if an out of range extrapolation is required a flat extension
-!>        of the table data is performed.
-!>
-FUNCTION Interp_2D(labels1,labels2,table,pp) RESULT(p)
-  REAL(SRK),INTENT(IN) :: labels1(:),labels2(:),table(:,:),pp(2)
-  REAL(SRK) :: p,f1(2),f2(2)
+!>        a desired point.
+!> @param labels1 axis labels at which data points in table are defined for 1st dimension
+!> @param labels2 axis labels at which data points in table are defined for 2nd dimension
+!> @param table contains data to be interpolated between defined at points
+!>        given in labels1 and labels2
+!> @param axis_p point at which interpolation needs to be done to return a value
+!> @param data_p interpolated data found through linear interpolation to the point axis_p
+FUNCTION Interp_2D(labels1,labels2,table,axis_p) RESULT(data_p)
+  REAL(SRK),INTENT(IN) :: labels1(:),labels2(:),table(:,:),axis_p(:)
+  REAL(SRK) :: data_p,f1(2),f2(2)
   INTEGER(SIK) :: i,j,N_i,N_j,i_p,j_p
 
-  CALL Get_points_and_weights(labels1,pp(1),f1,i_p,N_i)
-  CALL Get_points_and_weights(labels2,pp(2),f2,j_p,N_j)
+  CALL Get_points_and_weights(labels1,axis_p(1),f1,i_p,N_i)
+  CALL Get_points_and_weights(labels2,axis_p(2),f2,j_p,N_j)
 
   !Get interpolated value
-  p=0.0_SRK
+  data_p=0.0_SRK
   DO j=MAX(j_p-1,1),MIN(j_p,N_j)
     DO i=MAX(i_p-1,1),MIN(i_p,N_i)
-      p=p+f1(i-i_p+2)*f2(j-j_p+2)*table(i,j)
+      data_p=data_p+f1(i-i_p+2)*f2(j-j_p+2)*table(i,j)
     ENDDO
   ENDDO
 
@@ -81,25 +87,30 @@ ENDFUNCTION Interp_2D
 !
 !-------------------------------------------------------------------------------
 !> @brief This routine takes a 2D data table and linearly interpolates to find
-!>        a desired point.input arrays and interpolates as needed. It is important
-!>        to note that if an out of range extrapolation is required a flat extension
-!>        of the table data is performed.
+!>        a desired point.
+!> @param labels1 axis labels at which data points in table are defined for 1st dimension
+!> @param labels2 axis labels at which data points in table are defined for 2nd dimension
+!> @param labels3 axis labels at which data points in table are defined for 3rd dimension
+!> @param table contains data to be interpolated between defined at points
+!>        given in labels1, labels2, and labels 3
+!> @param axis_p point at which interpolation needs to be done to return a value
+!> @param data_p interpolated data found through linear interpolation to the point axis_p
 !>
-FUNCTION Interp_3D(labels1,labels2,labels3,table,pp) RESULT(p)
-  REAL(SRK),INTENT(IN) :: labels1(:),labels2(:),labels3(:),table(:,:,:),pp(3)
-  REAL(SRK) :: p,f1(2),f2(2),f3(2)
+FUNCTION Interp_3D(labels1,labels2,labels3,table,axis_p) RESULT(data_p)
+  REAL(SRK),INTENT(IN) :: labels1(:),labels2(:),labels3(:),table(:,:,:),axis_p(:)
+  REAL(SRK) :: data_p,f1(2),f2(2),f3(2)
   INTEGER(SIK) :: i,j,k,N_i,N_j,N_k,i_p,j_p,k_p
 
-  CALL Get_points_and_weights(labels1,pp(1),f1,i_p,N_i)
-  CALL Get_points_and_weights(labels2,pp(2),f2,j_p,N_j)
-  CALL Get_points_and_weights(labels3,pp(3),f3,k_p,N_k)
+  CALL Get_points_and_weights(labels1,axis_p(1),f1,i_p,N_i)
+  CALL Get_points_and_weights(labels2,axis_p(2),f2,j_p,N_j)
+  CALL Get_points_and_weights(labels3,axis_p(3),f3,k_p,N_k)
 
   !Get interpolated value
-  p=0.0_SRK
+  data_p=0.0_SRK
   DO k=MAX(k_p-1,1),MIN(k_p,N_k)
     DO j=MAX(j_p-1,1),MIN(j_p,N_j)
       DO i=MAX(i_p-1,1),MIN(i_p,N_i)
-        p=p+f1(i-i_p+2)*f2(j-j_p+2)*f3(k-k_p+2)*table(i,j,k)
+        data_p=data_p+f1(i-i_p+2)*f2(j-j_p+2)*f3(k-k_p+2)*table(i,j,k)
       ENDDO
     ENDDO
   ENDDO
@@ -108,13 +119,18 @@ ENDFUNCTION Interp_3D
 !
 !-------------------------------------------------------------------------------
 !> @brief This routine takes a 1D array of table labels (the points along a single
-!>        dimension at which table entries are defined) the interpolation point
+!>        dimension at which table entries are defined) and the interpolation point
 !>        coordinate along that dimension and returns the index of the label entry
 !>        directly ahead of that point along with the weights of that point and the
-!>        point before it.
+!>        preceeding point.
+!> @param labels axis labels at which data points in table are defined
+!> @param axis_p point at which interpolation needs to be done to return a value
+!> @param f weights associated with the points on either side of axis_p
+!> @param i_p index of point directly ahead of axis_p in labels
+!> @param N_i number of entries of labels
 !>
-SUBROUTINE Get_points_and_weights(labels,pp,f,i_p,N_i)
-  REAL(SRK),INTENT(IN) :: labels(:),pp
+SUBROUTINE Get_points_and_weights(labels,axis_p,f,i_p,N_i)
+  REAL(SRK),INTENT(IN) :: labels(:),axis_p
   REAL(SRK),INTENT(OUT) :: f(2)
   INTEGER(SIK),INTENT(OUT) :: i_p,N_i
   INTEGER(SIK) :: i
@@ -125,19 +141,19 @@ SUBROUTINE Get_points_and_weights(labels,pp,f,i_p,N_i)
   IF(labels(1) < labels(2)) THEN
     !Acending order
     DO i_p=1,N_i
-      IF(pp < labels(i_p)) EXIT
+      IF(axis_p < labels(i_p)) EXIT
     ENDDO
     IF(i_p > 1 .AND. i_p < N_i+1) THEN
-      f(1)=(labels(i_p)-pp)/(labels(i_p)-labels(i_p-1))
+      f(1)=(labels(i_p)-axis_p)/(labels(i_p)-labels(i_p-1))
       f(2)=1.0_SRK-f(1)
     ENDIF
   ELSE
     !Decending order
     DO i_p=1,N_i
-      IF(pp > labels(i_p)) EXIT
+      IF(axis_p > labels(i_p)) EXIT
     ENDDO
     IF(i_p > 1 .AND. i_p < N_i+1) THEN
-      f(1)=(pp-labels(i_p))/(labels(i_p-1)-labels(i_p))
+      f(1)=(axis_p-labels(i_p))/(labels(i_p-1)-labels(i_p))
       f(2)=1.0_SRK-f(1)
     ENDIF
   ENDIF
