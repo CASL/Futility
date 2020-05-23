675e6cc4441e02e0aba79c6fa8038158a4435717
Benjamin Collins <collinsbs@ornl.gov>
2020-05-22 23:10:36 -0400

    Adds fix that enables openmp in kokkos if the project enables openmp

--- cmake/ProjectCompilerPostConfig.cmake
+++ cmake/ProjectCompilerPostConfig.cmake
@@ -1,11 +1,11 @@
 # Set Kokkos host spaces
 IF(${PROJECT_NAME}_ENABLE_Kokkos)
   SET(KOKKOS_ENABLE_SERIAL TRUE)
-  IF(${PROJECT_NAME}_ENABLE_OpenMP)
-    SET(KOKKOS_ENABLE_OPENMP TRUE)
+  IF(DEFINED ${PROJECT_NAME}_ENABLE_OpenMP)
+    SET(KOKKOS_ENABLE_OPENMP ${${PROJECT_NAME}_ENABLE_OpenMP})
   ENDIF()
 ENDIF()
 
-if(Trilinos_SOURCE_DIR)
+IF(Trilinos_SOURCE_DIR)
   include(${Trilinos_SOURCE_DIR}/cmake/ProjectCompilerPostConfig.cmake)
-endif()
+ENDIF()
